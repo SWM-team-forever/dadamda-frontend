f268446f51717ce671ed32c398e46f7de8ca6b76
ShinYoung-Kim <lukey0515@naver.com>
2023-07-03 16:40:04 +0900

    [DAD-359] feat: 로그인, 회원가입 페이지 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import './App.css'
+import { GoogleLoginButton } from 'react-social-login-buttons'
+import SignUp from './pages/SignUp'
+import Login from './pages/Login'
 
 function App() {
   return (
     <>
       
+      <GoogleLoginButton>
+        구글로 로그인하기
+      </GoogleLoginButton>
+      <GoogleLoginButton>
+        구글로 회원가입하기
+      </GoogleLoginButton>
+
+      <Login/>
+      <SignUp/>
     </>
   )
 }
