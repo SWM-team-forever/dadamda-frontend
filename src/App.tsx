5cd4153600f31de104ed9686ee810baf8602c579
ShinYoung-Kim <lukey0515@naver.com>
2023-07-03 16:50:10 +0900

    [DAD-359] feat: 배경색 지정

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,10 @@
 import './App.css'
-import { GoogleLoginButton } from 'react-social-login-buttons'
-import SignUp from './pages/SignUp'
-import Login from './pages/Login'
+import LoginOrRegister from './pages/LoginOrRegister'
 
 function App() {
   return (
     <>
-      
-      <GoogleLoginButton>
-        구글로 로그인하기
-      </GoogleLoginButton>
-      <GoogleLoginButton>
-        구글로 회원가입하기
-      </GoogleLoginButton>
-
-      <Login/>
-      <SignUp/>
+      <LoginOrRegister/>
     </>
   )
 }
---
+++ src/pages/LoginOrRegister.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from 'styled-components';
+
+function LoginOrRegister() {
+  return (
+    <Container>
+
+    </Container>
+  )
+}
+
+const Container = styled.div`
+    background-color: #dcdefe;
+    width: 100vw;
+    height: 100vh;
+`
+
+export default LoginOrRegister
