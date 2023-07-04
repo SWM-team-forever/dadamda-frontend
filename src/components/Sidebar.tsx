6ee1416b90f49ae435989a2d1b49b4404ca6b8fc
ShinYoung-Kim <lukey0515@naver.com>
2023-07-04 12:06:47 +0900

    [DAD-350] feat: sidebar 파일 분리

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import { MdOutlineDashboard, MdProductionQuantityLimits, MdOutlineArticle, MdOutlineMap, MdOutlineOndemandVideo, MdAccessibility } from 'react-icons/md';
+import styled from 'styled-components';
+
+function Sidebar() {
+  return (
+    <SidebarWrapper>
+      <Board>
+                <MdOutlineDashboard style={{ width: "3rem", height: "3rem" }}/>
+                <span>보드</span>
+            </Board>
+            <Product>
+                <MdProductionQuantityLimits style={{ width: "3rem", height: "3rem" }}/>
+                <span>상품</span>
+            </Product>
+            <Article>
+                <MdOutlineArticle style={{ width: "3rem", height: "3rem" }}/>
+                <span>아티클</span>
+            </Article>
+            <Map>
+                <MdOutlineMap style={{ width: "3rem", height: "3rem" }}/>
+                <span>지도</span>
+            </Map>
+            <Video>
+                <MdOutlineOndemandVideo style={{ width: "3rem", height: "3rem" }}/>
+                <span>비디오</span>
+            </Video>
+            <Etc>
+                <MdAccessibility style={{ width: "3rem", height: "3rem" }}/>
+                <span>기타</span>
+            </Etc>
+    </SidebarWrapper>
+  )
+}
+
+const SidebarWrapper = styled.div`
+    background-color: white;
+    display: flex;
+    flex-direction: column;
+    width: fit-content;
+    padding: 10px;
+`
+
+const Board = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+const Product = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+
+const Article = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+const Map = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+const Video = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+const Etc = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+
+export default Sidebar;
--- src/pages/CardViewPage.tsx
+++ src/pages/CardViewPage.tsx
@@ -1,37 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
-import { MdOutlineDashboard, MdProductionQuantityLimits, MdOutlineArticle, MdOutlineMap, MdOutlineOndemandVideo, MdAccessibility } from 'react-icons/md';
 import ProductTemplate from '../components/ProductTemplate';
+import Sidebar from '../components/Sidebar';
 
 function CardViewPage() {
   return (
     <Container>
-        <Sidebar>
-            <Board>
-                <MdOutlineDashboard style={{ width: "3rem", height: "3rem" }}/>
-                <span>보드</span>
-            </Board>
-            <Product>
-                <MdProductionQuantityLimits style={{ width: "3rem", height: "3rem" }}/>
-                <span>상품</span>
-            </Product>
-            <Article>
-                <MdOutlineArticle style={{ width: "3rem", height: "3rem" }}/>
-                <span>아티클</span>
-            </Article>
-            <Map>
-                <MdOutlineMap style={{ width: "3rem", height: "3rem" }}/>
-                <span>지도</span>
-            </Map>
-            <Video>
-                <MdOutlineOndemandVideo style={{ width: "3rem", height: "3rem" }}/>
-                <span>비디오</span>
-            </Video>
-            <Etc>
-                <MdAccessibility style={{ width: "3rem", height: "3rem" }}/>
-                <span>기타</span>
-            </Etc>
-        </Sidebar>
+        <Sidebar/>
         
         <CardArea>
             <ProductTemplate/>
@@ -48,45 +23,7 @@ const Container = styled.div`
     display: flex;
 `
 
-const Sidebar = styled.div`
-    background-color: white;
-    display: flex;
-    flex-direction: column;
-    width: fit-content;
-    padding: 10px;
-`
 
-const Board = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-`
-const Product = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-`
-
-const Article = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-`
-const Map = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-`
-const Video = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-`
-const Etc = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-`
 
 const CardArea = styled.div`
 
