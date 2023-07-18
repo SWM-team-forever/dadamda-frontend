3707055bb21bbb7d95f6033d8183d5815a395b84
ShinYoung-Kim <lukey0515@naver.com>
2023-07-18 20:25:22 +0900

    [DAD-408] feat: 로고 추가

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import styled from 'styled-components';
 import theme from '../assets/styles/theme';
+import logo from '../assets/logo.png';
+
 
 function MainPage() {
   return (
     <>
         <BackgroundContainer>메인 페이지</BackgroundContainer>
+        <LogoImg src={logo}/>
     </>
   )
 }
@@ -14,6 +17,11 @@ const BackgroundContainer = styled.div`
     width: 100vw;
     height: 100vh;
     background-color: ${theme.color.background_color};
+`
+
+const LogoImg = styled.img`
+    height: 25px;
+    width: 125px;
 `
 
 export default MainPage;
