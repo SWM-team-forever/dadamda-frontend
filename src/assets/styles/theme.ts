646cca8df4e617e016bd1eb872573f7328213148
ShinYoung-Kim <lukey0515@naver.com>
2023-07-18 19:53:48 +0900

    [DAD-409] feat: 테마 파일 생성

---
+++ src/assets/styles/theme.ts
@@ -0,0 +1,18 @@
+const theme = {
+    color: {
+        background_color: '#EFF1F3',
+        primary_color: '#154FEF',
+        secondary_color: 'rgba(179, 212, 255, 0.5)',
+        text_gray_color: '#44546F',
+        selected_gray_color: 'rgba(68, 84, 111, 0.1)',
+        icon_color: '#626F86',
+        primary_opacity_color: 'rgba(21, 79, 239, 0.8)',
+    },
+    media: {
+        small: 479,
+        medium: 639,
+        large: 1023,
+    },
+}
+
+export default theme;
