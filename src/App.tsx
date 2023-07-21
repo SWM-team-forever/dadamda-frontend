94e539fb90069e8b628ce34d808947f9614a31e6
ShinYoung-Kim <lukey0515@naver.com>
2023-07-21 13:25:04 +0900

    [DAD-408] feat: 유저 페이지 생성

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,21 @@ import './App.css'
 import AuthPage from './pages/AuthPage';
 import ScrapPage from './pages/ScrapPage';
 import Header from './components/molcules/Header';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './pages/MainPage';
 import { USER } from './config';
+import UserPage from './pages/UserPage';
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <Header user={USER} size='large'/>
+        <Header user={USER} size='large' />
         <Routes>
-          <Route path='/' element={<MainPage/>}></Route>
-          <Route path='/main' element={<MainPage/>}></Route>
-          <Route path='/user' element={<ScrapPage/>}></Route>
-          <Route path='/scrap' element={<ScrapPage/>}></Route>
+          <Route path='/' element={<MainPage />}></Route>
+          <Route path='/main' element={<MainPage />}></Route>
+          <Route path='/user' element={<UserPage />}></Route>
+          <Route path='/scrap' element={<ScrapPage />}></Route>
         </Routes>
       </BrowserRouter>
     </>
