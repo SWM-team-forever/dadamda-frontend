6bdbd30e41072d21be9d199628a1821b28e6255f
ShinYoung-Kim <lukey0515@naver.com>
2023-07-25 18:42:25 +0900

    [DAD-470] 기타 스크랩 카드 생성 및 스토리북 연결

---
+++ src/components/organisms/OtherScrapCard.tsx
@@ -0,0 +1,71 @@
+import styled from 'styled-components';
+
+import Button from '../atoms/DefaultButton';
+import MoreIcon from '../../assets/icons/MoreVerticalIcon.png';
+
+interface OtherScrapCardProps {
+    pageUrl: string,
+    title: string,
+    description: string,
+    thumbnailURL: string,
+    scrapCreatedDate: string,
+}
+
+function OtherScrapCard(props: OtherScrapCardProps) {
+    return (
+        <CardWrapper>
+            <CardImage src={props.thumbnailURL} />
+            <CardInfoWrapper>
+                <EmpasizedTypography>{props.title}</EmpasizedTypography>
+                <DefaultTypography>{props.description}</DefaultTypography>
+            </CardInfoWrapper>
+            <ButtonContainer>
+                <Button buttonStyle={'gray'} label={'메모 추가하기'} fullWidth isRound />
+                <MoreIconContainer src={MoreIcon} />
+            </ButtonContainer>
+        </CardWrapper>
+    );
+}
+
+const CardWrapper = styled.div`
+    max-width: 200px;
+    padding: 15px;
+    display: flex;
+    flex-direction: column;
+    gap: 15px;
+    background-color: white;
+    border-radius: 4px;
+`
+
+const CardImage = styled.img<{ thumbnailURL: string }>`
+    width: 100%;
+    height: 140px;
+    background: url(${props => props.thumbnailURL}), cover no-repeat;
+    border-radius: 4px;
+`
+
+const CardInfoWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+`
+
+const EmpasizedTypography = styled.span`
+    font-size: 20px;
+    font-weight: bold;
+`
+
+const DefaultTypography = styled.span`
+    font-size: 14px;
+`
+
+const ButtonContainer = styled.div`
+    display: flex;
+`
+
+const MoreIconContainer = styled.img`
+    width: 24px;
+    height: 24px;
+`
+
+export default OtherScrapCard;
