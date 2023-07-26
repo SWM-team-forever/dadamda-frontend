d14d3d2807eeae85046a038a13eac0320ddf99c6
ShinYoung-Kim <lukey0515@naver.com>
2023-07-26 18:14:53 +0900

    [DAD-484] feat: flex 가진 컨테이너 컴포넌트 생성

---
+++ src/components/atoms/ColumnContainer.tsx
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+interface ColumnContainer {
+    children: React.ReactNode,
+    style: object,
+}
+
+function ColumnContainer({ children, style }: ColumnContainer) {
+    return (
+        <Wrapper style={{ ...style }}>
+            {children}
+        </Wrapper>
+    );
+}
+
+const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+`
+
+export default ColumnContainer;
