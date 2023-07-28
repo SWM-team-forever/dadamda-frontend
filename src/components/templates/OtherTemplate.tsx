e4e696ded2b3df7737281f88ceb5868cfbd29287
ShinYoung-Kim <lukey0515@naver.com>
2023-07-28 15:54:55 +0900

    [DAD-472] refactor: 불필요한 로그 제거

--- src/components/templates/OtherTemplate.tsx
+++ src/components/templates/OtherTemplate.tsx
@@ -39,7 +39,6 @@ function OtherTemplate() {
             }).then((response) => response.json())
                 .then((data) => {
                     setOthers(data.data.content);
-                    console.log(data.data.content);
                 })
                 .catch(err => console.error(err));
     }, [token]);
