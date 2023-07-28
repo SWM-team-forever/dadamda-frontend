f64e3c1ff29a3ce0c3fdc2238520b940f62c4882
ShinYoung-Kim <lukey0515@naver.com>
2023-07-28 11:00:46 +0900

    [DAD-462] feat: icon 버튼 컴포넌트 추가

--- src/components/atoms/DefaultButton.tsx
+++ src/components/atoms/DefaultButton.tsx
@@ -5,38 +5,48 @@ interface ButtonProps {
     buttonStyle: 'primary' | 'secondary' | 'gray' | 'text-only',
     label: string;
     isRound?: boolean;
-    endIcon?: React.ReactElement;
-    startIcon?: React.ReactElement;
+    endIcon?: string;
+    startIcon?: string;
     fullWidth?: boolean;
-    variant?: string;
-    color?: string;
-    size?: string;
+    size?: 'small' | 'medium' | 'large',
     onClick?: () => void;
 }
 
-function Button({ label, buttonStyle, isRound, fullWidth, onClick
+const sizeMapping = {
+    'small': 12,
+    'medium': 24,
+    'large': 36,
+}
+
+function Button({ label, buttonStyle, isRound, fullWidth, onClick, startIcon, endIcon, size,
 }: ButtonProps) {
     return (
-        <ButtonContainer buttonStyle={buttonStyle} fullWidth={fullWidth} isRound={isRound} onClick={onClick}>
+        <ButtonContainer buttonStyle={buttonStyle} fullWidth={fullWidth} isRound={isRound} onClick={onClick} size={size}>
+            {startIcon && <Icon src={startIcon} size={size} />}
             {label}
+            {endIcon && <Icon src={endIcon} size={size} />}
         </ButtonContainer>
     );
 }
 
-const ButtonContainer = styled.button<{ buttonStyle: string, fullWidth?: boolean, isRound?: boolean }>`
+const ButtonContainer = styled.button<{ buttonStyle: string, fullWidth?: boolean, isRound?: boolean, size?: string }>`
     ${props => getButtonStyle(props.buttonStyle)};
     ${props => props.fullWidth && 'width: 100%'};
     ${props => props.isRound && 'border-radius: 4px'};
     border: none;
     cursor: pointer;
     padding: 5px 12px;
+    ${props => props.size && getFontSize(props.size)};
+    display: flex;
+    justify-content: center;
+    align-itmes: center;
 `
 
 const getButtonStyle = (buttonStyle: string) => {
     switch (buttonStyle) {
         case 'primary':
             return `
-    background-color: ${theme.color.primary_color};
+                background-color: ${theme.color.primary_color};
                 color: white;
 `
         case 'secondary':
@@ -56,5 +66,46 @@ const getButtonStyle = (buttonStyle: string) => {
 `
     }
 }
+
+const getIconSize = (size: string | undefined) => {
+    switch (size) {
+        case 'small':
+            return `
+            width: 12px;
+            height: 12px;
+            `;
+        case 'medium':
+            return `
+            width: 24px;
+            height: 24px;
+            `;
+        case 'large':
+            return `
+            width: 36px;
+            height: 36px;
+            `
+    }
+}
+
+const getFontSize = (size: string) => {
+    switch (size) {
+        case 'small':
+            return `
+            font-size: 12px;
+            `;
+        case 'medium':
+            return `
+            font-size: 24px;
+            `;
+        case 'large':
+            return `
+            font-size: 36px;
+            `
+    }
+}
+
+const Icon = styled.img<{ size?: string }>`
+    ${props => getIconSize(props.size)}
+`
 
 export default Button;
