9344794c946ab416991a8282315d907e107d021c
ShinYoung-Kim <lukey0515@naver.com>
2023-08-10 15:39:34 +0900

    [DAD-566] feat: 썸네일 이미지 컴포넌트 생성

---
+++ src/components/atoms/ThumbnailImage.tsx
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+interface ThumbnailImageProps {
+    thumbnailUrl: string,
+}
+
+function ThumbnailImage({ thumbnailUrl }: ThumbnailImageProps) {
+    return (
+        <CardImageWrapper>
+            <CardImage src={thumbnailUrl} />
+        </CardImageWrapper>
+    );
+}
+
+const CardImageWrapper = styled.div`
+   width: 100%;
+   aspect-ratio: 16 / 9;
+   overflow: hidden;
+   position: relative;
+`
+
+const CardImage = styled.img`
+    position: absolute;
+    width: 100%;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+`
+
+export default ThumbnailImage;
