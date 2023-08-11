b8098957712918c7b54d489b5bb191be1c42cda1
ShinYoung-Kim <lukey0515@naver.com>
2023-08-11 12:03:08 +0900

    [DAD-566] feat: 대략적인 UI 완성

--- src/components/organisms/ExistVideoScrapContainer.tsx
+++ src/components/organisms/ExistVideoScrapContainer.tsx
@@ -3,8 +3,14 @@ import RowContainer from '../atoms/RowContainer';
 import ColumnContainer from '../atoms/ColumnContainer';
 import theme from '../../assets/styles/theme';
 import { contentProps } from '../../types/ContentType';
-import { Box, Card, CardActionArea, CardContent, CardMedia, Typography } from '@mui/material';
+import { Autocomplete, Box, Card, CardActionArea, CardContent, CardMedia, TextareaAutosize, Typography } from '@mui/material';
 import ThumbnailImage from '../atoms/ThumbnailImage';
+import IconButton from '../atoms/IconButton';
+import { MoreIcon, ShortcutIcon } from '../atoms/Icon';
+import ProfileImage from '../atoms/ProfileImage';
+import { getTimeDiff } from '../../hooks/useCalculateDateDiff';
+import Memo from '../molcules/Memo';
+import Button from '../atoms/DefaultButton';
 
 interface ExistVideoScrapContainerProps {
     contents: contentProps["content"][],
@@ -13,14 +19,16 @@ interface ExistVideoScrapContainerProps {
 }
 
 
-function VideoItemSummary({ thumbnailUrl, title, channelName }) {
+function VideoItemSummary({ thumbnailUrl, title, channelName, focusedContent }) {
 
     return (
-        <Card sx={{
-            width: '100%',
-            boxShadow: 'none',
-            borderRadius: '0',
-        }}>
+        <div
+            style={{
+                width: '100%',
+                boxShadow: 'none',
+                borderRadius: '0',
+                display: 'block',
+            }}>
             <CardActionArea
                 sx={{
                     display: 'flex',
@@ -55,53 +63,201 @@ function VideoItemSummary({ thumbnailUrl, title, channelName }) {
                     </Typography>
                 </CardContent>
             </CardActionArea>
-        </Card>
+
+        </div>
     )
+}
+
+function FocusedVideoItem({ focusedContent }: contentProps['content']) {
+
+    return (
+        <div
+            style={{
+                position: 'relative',
+                width: '100%',
+                paddingBottom: '56.25%',
+            }}>
+            <iframe src={focusedContent.embedUrl}
+                style={{
+                    position: 'absolute',
+                    width: '100%',
+                    height: '100%',
+                }} />
+        </div>
+    );
 }
 
 function ExistVideoScrapContainer({ contents }: ExistVideoScrapContainerProps) {
     return (
         <RowContainer
             style={{
-                padding: '20px',
+                padding: '0 20px',
                 gap: '10px',
                 boxSizing: 'border-box',
                 width: '100%',
+                height: '100%',
             }}
         >
             <VideoListWrapper>
-                <Typography
-                    style={{
-                        margin: '0 10px',
-                        fontSize: '0.875rem',
-                        color: theme.color.icon_color,
-                    }}
-                >비디오 목록</Typography>
                 <VideoList>
                     {contents.map((content) => {
                         const { thumbnailUrl, title, channelName } = content;
-                        return <VideoItemSummary thumbnailUrl={thumbnailUrl} title={title} channelName={channelName} />
+                        return <VideoItemSummary thumbnailUrl={thumbnailUrl} title={title} channelName={channelName} focusedContent={contents[0]} />
                     })}
                 </VideoList>
+                <MemoContainer focusedContent={contents[3]} />
             </VideoListWrapper>
+            <Card sx={{
+                width: '100%',
+                height: 'fit-content',
+                marginBottom: '20px',
+            }}>
+                <FocusedVideoItem focusedContent={contents[1]} />
+                <FocusedVideoItemDetails focusedContent={contents[1]} />
+            </Card>
+        </RowContainer >
+    )
+}
 
-        </RowContainer>
+function FocusedVideoItemDetails({ focusedContent }) {
+    const videoMenus = [{
+        title: '게시일',
+        content: focusedContent.publishedDate,
+    }, {
+        title: '조회수',
+        content: focusedContent.watchedCnt,
+    }, {
+        title: '영상 길이',
+        content: focusedContent.playTime,
+    },]
+    return (
+        <ColumnContainer
+            style={{
+                gap: '10px',
+                padding: '30px',
+                boxSizing: 'border-box',
+                width: '100%',
+                background: 'white',
+            }}>
+            <RowContainer
+                style={{
+                    gap: '5px',
+                    width: '100%',
+                    boxSizing: 'border-box',
+                    justifyContent: 'space-between',
+                }}>
+                <ColumnContainer>
+                    <Typography
+                        sx={{
+                            wordBreak: 'break-all',
+                        }}>
+                        {focusedContent.siteName}
+                    </Typography>
+                    <Typography
+                        sx={{
+                            fontSize: '1.25rem',
+                            lineHeight: '120%',
+                        }}>
+                        {focusedContent.title}
+                    </Typography>
+                </ColumnContainer>
+                <RowContainer style={{ gap: '5px' }}>
+                    <ShortcutIcon size='24' fill={theme.color.icon_color} />
+                    <MoreIcon size='24' fill={theme.color.icon_color} />
+                </RowContainer>
+            </RowContainer>
+            <RowContainer
+                style={{
+                    gap: '10px',
+                }}>
+                <ProfileImage size={24} source={focusedContent.channelImageUrl} />
+                <Typography>{focusedContent.channelName}</Typography>
+            </RowContainer>
+            <RowContainer style={{ justifyContent: 'space-between' }}>
+                {videoMenus.map(menu => {
+                    return (
+                        <>
+                            {menu.content &&
+                                <ColumnContainer style={{ alignItems: 'center', flex: '1' }}>
+                                    <Typography>{
+                                        typeof (menu.content) === 'number' ? getTimeDiff(menu.content) : menu.content
+                                    }</Typography>
+                                    <Typography>{menu.title}</Typography>
+                                </ColumnContainer>
+                            }
+                        </>
+                    )
+                })}
+            </RowContainer>
+            <RowContainer
+                style={{
+                    width: '100%',
+                    whiteSpace: 'pre-wrap',
+                }}>
+                {focusedContent.description}
+            </RowContainer>
+        </ColumnContainer>
+    )
+}
+
+function MemoContainer({ focusedContent }) {
+    return (
+        <ColumnContainer
+            style={{
+                gap: '10px',
+                backgroundColor: 'white',
+                borderRadius: '4px',
+                padding: '10px',
+                height: 'calc(50% - 5px)',
+                width: '100%',
+                overflow: 'auto',
+                boxSizing: 'border-box',
+                whiteSpace: 'pre-wrap',
+                wordBreak: 'break-all',
+            }}>
+            {
+                focusedContent.memoList?.map(memo => {
+                    return <Memo memoImageURL={memo.memoImageUrl} memoText={memo.memoText} />
+                })
+            }
+            < EditText placeholder="메모를 입력해주세요." onChange={() => console.log('hi')} />
+            <TextareaAutosize placeholder='메모를 입력해주세요.' />
+        </ColumnContainer >
     )
 }
 
 const VideoList = styled.section`
     width: 100%;
+    height: calc(50% - 5px);
     box-sizing: border-box;
     display: flex;
     flex-direction: column;
+    background-color: white;
+    box-shadow: ${theme.style.shadow};
+    border-radius: 4px;
+    overflow: auto;
 `
 
 const VideoListWrapper = styled.div`
-    width: 250px;
-    background-color: white;
-    padding: 10px;
+    width: 400px;
+    padding: 0 10px;
+    height: calc(100% - 74px);
+    overflow: auto;
+    box-sizing: border-box;
+    position: sticky;
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+`
+const EditText = styled.textarea`
+    font-size: 12px;
     border-radius: 4px;
-    box-shadow: ${theme.style.shadow};
+    padding: 15px;
+    background-color: ${theme.color.background_color};
+    border: none;
+    width: 100%;
+    box-sizing: border-box;
+    resize: none;
 `
 
 export default ExistVideoScrapContainer
