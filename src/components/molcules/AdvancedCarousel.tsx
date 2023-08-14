51ddcdde8e8588e59bdc01f7e2d6f3fe625184d8
ShinYoung-Kim <lukey0515@naver.com>
2023-08-14 12:31:22 +0900

    [DAD-567] feat: 화살표로 캐러셀 움직임 제어 기능 추가

--- src/components/molcules/AdvancedCarousel.tsx
+++ src/components/molcules/AdvancedCarousel.tsx
@@ -1,42 +1,49 @@
 import { useSnapCarousel } from 'react-snap-carousel';
 import CategoryItemHorizontal from '../organisms/CategoryItemHorizontal';
 import ProductCategoryItemVertical from '../organisms/ProductCategoryItemVertical';
+import RowContainer from '../atoms/RowContainer';
+import { LeftArrowIcon, RightArrowIcon } from '../atoms/Icon';
+import theme from '../../assets/styles/theme';
 
 const AdvancedCarousel = ({ contents }) => {
     const { scrollRef, pages, activePageIndex, next, prev, goTo } =
         useSnapCarousel();
     return (
-        <>
+        <RowContainer
+            style={{
+                justifyContent: 'space-between',
+            }}>
+            <div
+                style={{
+                    display: 'flex',
+                    alignItems: 'center',
+                }}
+                onClick={() => prev()}>
+                <LeftArrowIcon size={'30'} fill={theme.color.icon_color} />
+            </div>
             <ul
                 ref={scrollRef}
                 style={{
                     display: 'flex',
                     overflow: 'auto',
-                    scrollSnapType: 'x mandatory'
+                    scrollSnapType: 'x mandatory',
+                    flex: '1',
+                    padding: '0 10px',
                 }}
             >
                 {contents.map((content) => (
                     <ProductCategoryItemVertical content={content} />
                 ))}
             </ul>
-            <div>
-                {activePageIndex + 1} / {pages.length}
+            <div
+                style={{
+                    display: 'flex',
+                    alignItems: 'center',
+                }}
+                onClick={() => next()}>
+                <RightArrowIcon size={'30'} fill={theme.color.icon_color} />
             </div>
-            <button onClick={() => prev()}>Prev</button>
-            <button onClick={() => next()}>Next</button>
-            <ol style={{ display: 'flex' }}>
-                {pages.map((_, i) => (
-                    <li key={i}>
-                        <button
-                            style={i === activePageIndex ? { opacity: 0.5 } : {}}
-                            onClick={() => goTo(i)}
-                        >
-                            {i + 1}
-                        </button>
-                    </li>
-                ))}
-            </ol>
-        </>
+        </RowContainer>
     );
 };
 
