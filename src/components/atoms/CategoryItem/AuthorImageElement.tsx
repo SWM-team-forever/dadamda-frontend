0ff2adfc4c6cebd9eb4b6337b8a00d901117e8c6
ShinYoung-Kim <lukey0515@naver.com>
2023-08-17 00:06:27 +0900

    [DAD-656] fix: 타입스크립트 오류 해결

--- src/components/atoms/CategoryItem/AuthorImageElement.tsx
+++ src/components/atoms/CategoryItem/AuthorImageElement.tsx
@@ -11,7 +11,7 @@ const siteNameStyles = {
     desktopArticleList: desktopArticleListStyle,
 }
 
-export function AuthorImageElement({ authorImage, varient }: any) {
+export function AuthorImageElement({ authorImage }: any) {
     return (
         <>
             {authorImage ? <ProfileImage size={30} source={authorImage} /> : <ProfileImage size={30} source={avatar} />}
