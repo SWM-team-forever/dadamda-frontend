70df55f44020836f2802b742e5bb5009d539ab3c
ShinYoung-Kim <lukey0515@naver.com>
2023-08-17 19:57:27 +0900

    [DAD-675] feat: 스크롤 영역 수정

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -21,109 +21,21 @@ function MainPage() {
   });
 
   return (
-    <>
+    <BackgroundContainer>
       <Hero />
       <Section />
       <Howto />
       <Footer />
-    </>
+    </BackgroundContainer>
   )
 }
-
 
 const BackgroundContainer = styled.div`    
     width: 100%;
     height: calc(100vh - 50px);
-    display: flex;
-    justify-content: center;
-    align-items: center;
-`
-const ContentContainer = styled.div`
-  width: 100%;
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-  @media screen and (max-width: 600px) {
-      flex-direction: column;
-  }
-  margin: 20px;
-`
-
-const TextContainer = styled.div`
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  gap: 20px;
-  margin: 20px;
-`
-
-const ImageContainer = styled.div`
-  width: 50%;
-  min-width: 300px;
-  aspect-ratio: 16/9;
-  background-color: ${theme.color.primary_color};
-`
-
-const EmpasizedTypography = styled.span`
-  font-size: 1.75rem;
-`
-
-const DefaultTypography = styled.span`
-  font-size: 0.875rem;
-`
-
-const Container = styled.div`
-  width: 100%;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: space-around;
-  margin: 20px;
-`
-
-const Content = styled.div`
-  width: 100%;
-  height: 80%;
-  display: flex;
-  @media screen and (max-width: 600px) {
-      flex-direction: column;
-  }
-`
-
-const ImageTextContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-  height: 100%;
-  align-items: center;
-  gap: 20px;
-  margin: 20px 0;
-`
-
-const Image = styled.div`
-  background-color: ${theme.color.primary_color};
-  width: 80%;
-  aspect-ratio: 1/1;
-`
-const LastContainer = styled.div`
-  width: 80%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: 20px;
-  padding: 20px 0;
-`
-
-const ButtonContainer = styled.div`
-  display: flex;
-  gap: 10px;
-`
-const TypographyGroup = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+    position: fixed;
+    top: 50px;
+    overflow: auto;
 `
 
 export default MainPage;
