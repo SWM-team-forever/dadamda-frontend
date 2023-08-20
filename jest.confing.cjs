4112e5b20089940da48541b10a9e954d197d2fc7
ShinYoung-Kim <lukey0515@naver.com>
2023-08-20 16:00:52 +0900

    [DAD-703] chore: ts-jest 적용

--- jest.confing.cjs
+++ jest.confing.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   moduleFileExtensions: ["js", "json", "jsx", "ts", "tsx", "json"],
   transform: {
-    "^.+\\.(js|jsx)?$": "babel-jest",
+    "^.+\\.(ts|tsx|js|jsx)?$": "ts-jest",
   },
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/$1",
@@ -14,4 +14,5 @@ module.exports = {
     "<rootDir>/(tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx))",
   ],
   transformIgnorePatterns: ["<rootDir>/node_modules/"],
+  setupFilesAfterEnv: ['<rootDir>/[path-to-file]/jest-setup.js'],
 };
