a673c0e6633e3e63b74a9a1b020b05115ae03cc2
ShinYoung-Kim <lukey0515@naver.com>
2023-09-05 14:55:33 +0900

    [DAD-797] style: mui 기본 색상 추가 및 typo 추가

--- src/assets/styles/themeMuiStyle.ts
+++ src/assets/styles/themeMuiStyle.ts
@@ -13,6 +13,54 @@ const theme = createTheme({
     },
     typography: {
         fontFamily: 'Pretendard',
+    },
+    components: {
+        MuiTypography: {
+            variants: [
+                {
+                    props: {variant: 'h1'},
+                    style: {
+                        fontSize: '22pt',
+                        lineHeight: '160%',
+                    }
+                },
+                {
+                    props: {variant: 'h2'},
+                    style: {
+                        fontSize: '18pt',
+                        lineHeight: '160%',
+                    }
+                },
+                {
+                    props: {variant: 'h3'},
+                    style: {
+                        fontSize: '16pt',
+                        lineHeight: '150%',
+                    }
+                },
+                {
+                    props: {variant: 'h4'},
+                    style: {
+                        fontSize: '14pt',
+                        lineHeihgt: '150%',
+                    }
+                },
+                {
+                    props: {variant: 'h5'},
+                    style: {
+                        fontSize: '13pt',
+                        lineHeihgt: '150%',
+                    }
+                },
+                 {
+                    props: {variant: 'h6'},
+                    style: {
+                        fontSize: '12pt',
+                        lineHeihgt: '150%',
+                    }
+                }
+            ]
+        }
     }
 });
 
