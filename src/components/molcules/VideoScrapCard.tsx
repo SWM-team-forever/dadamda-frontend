238bf89d888925ed9dc65fb9222c5032904510b2
ShinYoung-Kim <lukey0515@naver.com>
2023-09-06 23:33:06 +0900

    [DAD-783] feat: 비디오 스크랩 카드 디자인 적용

--- src/components/molcules/VideoScrapCard.tsx
+++ src/components/molcules/VideoScrapCard.tsx
@@ -19,6 +19,12 @@ import { VideoInfosElement } from '../atoms/CategoryItem/VideoInfosElement';
 import { ChannelProfileElement } from '../atoms/CategoryItem/ChannelProfileElement';
 import { DescriptionElement } from '../atoms/CategoryItem/DescrptionElement';
 import { ThumbnailElement } from '../atoms/CategoryItem/ThumbnailElement';
+import { Box, Typography } from '@mui/material';
+import MemoCreateButton from '../atoms/CategoryItem/MemoCreateButton';
+import { ScrapCardSeeMoreIcon } from '../atoms/Icon';
+import { AuthorElement } from '../atoms/CategoryItem/AuthorElement';
+import { PublishedDateElement } from '../atoms/CategoryItem/PublishedDateElement';
+import { ChannelElement } from '../atoms/CategoryItem/ChannelElement';
 
 interface VideoScrapCardProps {
     content: contentProps['content'],
@@ -36,77 +42,114 @@ function VideoScrapCard({ content, showMemoCreateModal, showTooltip }: VideoScra
                 e.stopPropagation();
                 window.open(`${content.pageUrl}`);
             }}>
-            {content.siteName && <SiteNameElement siteName={content.siteName} varient={varient} />}
+            <Box
+                component='div'
+                onClick={
+                    (e) => {
+                        e.stopPropagation();
+                        showTooltip();
+                    }}
+                sx={{
+                    width: '100%',
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                }}
+            >
+                {content.siteName && <SiteNameElement siteName={content.siteName} varient={varient} />}
+                <ScrapCardSeeMoreIcon width='16' height='16' fill='#24292E' />
+            </Box>
             {content.title && <TitleElement title={content.title} varient={varient} />}
+            <RowContainer
+                style={{
+                    gap: '8px',
+                }}
+            >
+                {content.channelImageUrl && <ProfileImage source={content.channelImageUrl} size={20} />}
+                <RowContainer>
+                    <ChannelElement channelName={content.channelName} varient={varient} />
+                    {' • '}
+                    <PublishedDateElement publishedDate={content.publishedDate} varient={varient} />
+                </RowContainer>
+            </RowContainer>
             <ThumbnailElement thumbnailUrl={content.thumbnailUrl} />
-            <ChannelProfileElement channelImageUrl={content.channelImageUrl} channelName={content.channelName} varient={varient} />
-            <VideoInfosElement publishedDate={content.publishedDate} watchedCnt={content.watchedCnt} playTime={content.playTime} varient={varient} />
+            <RowContainer>
+                <ColumnContainer
+                    style={{
+                        gap: '4px',
+                        flex: '1',
+                    }}
+                >
+                    <Typography
+                        variant='h6'
+                        color={theme.color.Gray_070}
+                    >
+                        조회수
+                    </Typography>
+                    <Typography
+                        variant='h3'
+                        color={theme.color.Gray_090}
+                        sx={{
+                            fontWeight: '600',
+                            lineHeight: '150%',
+                        }}
+                    >
+                        {content.watchedCnt}
+                    </Typography>
+                </ColumnContainer>
+                <ColumnContainer
+                    style={{
+                        gap: '4px',
+                        flex: '1',
+                    }}
+                >
+                    <Typography
+                        variant='h6'
+                        color={theme.color.Gray_070}
+                    >
+                        영상 길이
+                    </Typography>
+                    <Typography
+                        variant='h3'
+                        color={theme.color.Gray_090}
+                        sx={{
+                            fontWeight: '600',
+                            lineHeight: '150%',
+                        }}
+                    >
+                        {content.playTime}
+                    </Typography>
+                </ColumnContainer>
+            </RowContainer>
             {content.description && <DescriptionElement description={content.description} varient={varient} />}
             {content.memoList?.map(memo => {
                 return <Memo memoImageURL={memo.memoImageUrl} memoText={memo.memoText} />
             })}
-            <ButtonContainer>
-                <Button buttonStyle={'gray'} label={'메모 추가하기'} fullWidth isRound onClick={(e) => {
-                    e.stopPropagation();
-                    showMemoCreateModal();
-                }} />
-                <MoreIconContainer src={MoreIcon} onClick={(e) => {
-                    e.stopPropagation();
-                    showTooltip();
-                }} />
-            </ButtonContainer>
+            <Box
+                component='div'
+                onClick={
+                    (e) => {
+                        e.stopPropagation();
+                    }}
+                sx={{
+                    width: '100%',
+                    display: 'flex',
+                    justifyContent: 'flex-end',
+                }}
+            >
+                <MemoCreateButton showMemoCreateModal={showMemoCreateModal} />
+            </Box>
         </CardWrapper>
     );
 }
 
 const CardWrapper = styled.div`
-            padding: 15px;
-            display: flex;
-            flex-direction: column;
-            gap: 10px;
-            background-color: white;
-            border-radius: 4px;
-            `
-
-const VideoPlayer = styled.iframe`
-            border-radius: 4px;
-            `
-
-const EmpasizedTypography = styled.span`
-    font-size: 20px;
-    font-weight: bold;
-    color: ${theme.color.text_gray_color};
-    overflow: hidden;
-    textOverflow: ellipsis;
-    display: -webkit-box;
-    -webkit-line-clamp: 2;
-    -webkit-box-orient: vertical;
-    wordWrap: break-word;
+    padding: 15px;
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
+    background: ${theme.color.Gray_020};
+    border-radius: 8px;
+    box-shadow: 0px 2px 16px 0px rgba(19, 48, 74, 0.08);  
 `
-
-const DefaultTypography = styled.span`
-font-size: 14px;
-color: ${theme.color.text_gray_color};
-`
-
-const DescriptionTypography = styled(DefaultTypography)`
-    overflow: hidden;
-    textOverflow: ellipsis;
-    display: -webkit-box;
-    -webkit-line-clamp: 3;
-    -webkit-box-orient: vertical;
-    wordWrap: break-word;
-`
-
-const ButtonContainer = styled.div`
-display: flex;
-align-items: center;
-`
-
-const MoreIconContainer = styled.img`
-            width: 24px;
-            height: 24px;
-            cursor: pointer;
-            `
 
 export default VideoScrapCard;
