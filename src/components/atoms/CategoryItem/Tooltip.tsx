458e6db9d73cadfae38b418ed64752758d868b66
ShinYoung-Kim <lukey0515@naver.com>
2023-09-08 10:34:28 +0900

    [DAD-802] feat: 툴팁 디자인 적용

--- src/components/atoms/CategoryItem/Tooltip.tsx
+++ src/components/atoms/CategoryItem/Tooltip.tsx
@@ -1,109 +1,93 @@
 import { styled, alpha } from '@mui/material/styles';
 import Menu, { MenuProps } from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
-import { Box } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import { ScrapCardSeeMoreIcon } from '../Icon';
 import { useState } from 'react';
-
-const StyledMenu = styled((props: MenuProps) => (
-    <Menu
-        elevation={0}
-        anchorOrigin={{
-            vertical: 'bottom',
-            horizontal: 'center',
-        }}
-        transformOrigin={{
-            vertical: 'top',
-            horizontal: 'center',
-        }}
-        {...props}
-    />
-))(({ theme }) => ({
-    '& .MuiPaper-root': {
-        borderRadius: 6,
-        marginTop: theme.spacing(1),
-        minWidth: 180,
-        color:
-            theme.palette.mode === 'light' ? 'rgb(55, 65, 81)' : theme.palette.grey[300],
-        boxShadow:
-            'rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px',
-        '& .MuiMenu-list': {
-            padding: '4px 0',
-        },
-        '& .MuiMenuItem-root': {
-            '& .MuiSvgIcon-root': {
-                fontSize: 18,
-                color: theme.palette.text.secondary,
-                marginRight: theme.spacing(1.5),
-            },
-            '&:active': {
-                backgroundColor: alpha(
-                    theme.palette.primary.main,
-                    theme.palette.action.selectedOpacity,
-                ),
-            },
-        },
-    },
-}));
+import theme from '../../../assets/styles/theme';
 
 export default function CustomizedMenus() {
     const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
     const open = Boolean(anchorEl);
-    const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    const handleOpenScrapEditOrDeleteMenu = (event: React.MouseEvent<HTMLElement>) => {
         setAnchorEl(event.currentTarget);
     };
-    const handleClose = () => {
+    const handleCloseScrapEditOrDeleteMenu = () => {
         setAnchorEl(null);
     };
 
     const menuItemContentList = [
         {
             title: '스크랩 수정',
+            clickAction: (e) => {
+                e.stopPropagation();
+                handleCloseScrapEditOrDeleteMenu();
+            }
         },
         {
             title: '스크랩 삭제',
+            clickAction: (e) => {
+                e.stopPropagation();
+                handleCloseScrapEditOrDeleteMenu();
+            }
         }
     ]
 
     return (
-        <div>
+        <Box>
             <Box
-                component='div'
-                onClick={
-                    (e) => {
-                        e.stopPropagation();
-                        handleClick(e);
-                    }}
                 sx={{
-                    width: '100%',
-                    display: 'flex',
-                    justifyContent: 'flex-end',
+                    width: '16px',
+                    height: '16px',
                 }}
-                id='스크랩 수정 및 삭제 툴팁 버튼'
-            >
+                onClick={(e) => {
+                    e.stopPropagation();
+                    handleOpenScrapEditOrDeleteMenu(e);
+                }}>
                 <ScrapCardSeeMoreIcon width='16' height='16' fill='#24292E' />
             </Box>
-            <StyledMenu
-                id="스크랩 수정 및 삭제 메뉴"
-                MenuListProps={{
-                    'aria-labelledby': '스크랩 수정 및 삭제 툴팁 버튼',
+            <Menu
+                sx={{
+                    '& .MuiPaper-root': {
+                        boxShadow: 'none',
+                        padding: '6px',
+                    },
+                    '& .MuiList-root': {
+                        padding: '0px',
+                    },
+                    mt: '24px',
+                    fill: '#FFF',
+                    filter: 'drop-shadow(0px 2px 16px rgba(19, 48, 74, 0.08))',
                 }}
+                id="menu-appbar"
                 anchorEl={anchorEl}
-                open={open}
-                onClose={handleClose}
+                anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'center',
+                }}
+                keepMounted
+                transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'center',
+                }}
+                open={Boolean(anchorEl)}
+                onClose={handleCloseScrapEditOrDeleteMenu}
             >
-                {menuItemContentList.map((item, index) => {
-                    return (
-                        <MenuItem
-                            key={index}
-                            onClick={handleClose}
-                            disableRipple
-                        >
-                            {item.title}
-                        </MenuItem>
-                    )
-                })}
-            </StyledMenu>
-        </div>
+                {menuItemContentList.map((menuItem) => (
+                    <MenuItem
+                        key={menuItem.title}
+                        onClick={menuItem.clickAction}
+                        sx={{
+                            '&:hover': {
+                                backgroundColor: '#F3F7FE',
+                            },
+                            padding: '6px 8px',
+                            color: theme.color.Blue_dry,
+                        }}>
+                        <Typography textAlign="center" variant='h5'>{menuItem.title}</Typography>
+                    </MenuItem>
+                ))}
+            </Menu>
+        </Box>
     );
 }
