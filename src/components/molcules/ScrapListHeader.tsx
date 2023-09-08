545a73a3ef7279f07494cd2ee41dc6401bf6703a
ShinYoung-Kim <lukey0515@naver.com>
2023-09-08 16:06:33 +0900

    [DAD-815] feat: 스크랩 리스트 헤더 컴포넌트 디자인 적용

--- src/components/molcules/ScrapListHeader.tsx
+++ src/components/molcules/ScrapListHeader.tsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import searchIcon from '../../assets/icons/SearchIcon.png';
 import theme from '../../assets/styles/theme';
 import useWarningSnackbar from '../../hooks/useWarningSnackbar';
+import { Box, Button, Typography } from '@mui/material';
 
 interface ScrapListHeaderProps {
     count: number,
@@ -11,51 +12,54 @@ interface ScrapListHeaderProps {
 
 function ScrapListHeader({ count, type }: ScrapListHeaderProps) {
     return (
-        <ScrapListHeaderWrapper>
-            <ScarpCountWrapper>
-                <EmpasizedTypography>{count} </EmpasizedTypography>
-                <DefaultTypography>개의 </DefaultTypography>
-                <EmpasizedTypography>{type} 스크랩</EmpasizedTypography>
-                <DefaultTypography>이 있습니다.</DefaultTypography>
-            </ScarpCountWrapper>
-            <SearchBar onClick={useWarningSnackbar}>
-                <SearchIconWrapper src={searchIcon} />
-                <EmpasizedTypography>Search</EmpasizedTypography>
-            </SearchBar>
-        </ScrapListHeaderWrapper>
+        <Box
+            sx={{
+                p: '24px',
+            }}
+        >
+            <Box
+                sx={{
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                }}
+            >
+                <Box
+                    sx={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        gap: '4px',
+                    }}
+                >
+                    <Typography
+                        variant='h1'
+                        color={theme.color.Gray_090}
+                        sx={{
+                            fontWeight: '600',
+                            lineHeight: '160%',
+                        }}
+                    >
+                        스크랩 {type}
+                    </Typography>
+                    <Typography
+                        variant='h6'
+                        color={theme.color.Gray_070}
+                        sx={{
+                            fontWeight: '600',
+                            lineHeight: '150%',
+                        }}
+                    >
+                        {count} 개
+                    </Typography>
+                </Box>
+                <Button
+                    color='primary'
+                    variant='contained'
+                >
+                    + 스크랩 추가
+                </Button>
+            </Box>
+        </Box>
     )
 }
-
-const ScrapListHeaderWrapper = styled.div`
-    display: flex;
-    padding: 20px;
-    justify-content: space-between;
-`
-
-const ScarpCountWrapper = styled.div`
-
-`
-
-const DefaultTypography = styled.span`
-    font-size: 14px;
-    color: ${theme.color.text_gray_color};
-`
-
-const EmpasizedTypography = styled.span`
-    font-size: 14px;
-    font-weight: bold;
-    color: ${theme.color.text_gray_color};
-`
-
-const SearchBar = styled.div`
-    display: flex;
-    align-items: center;
-    gap: 10px;
-`
-
-const SearchIconWrapper = styled.img`
-    width: 24px;
-    height: 24px;
-`
 
 export default ScrapListHeader
