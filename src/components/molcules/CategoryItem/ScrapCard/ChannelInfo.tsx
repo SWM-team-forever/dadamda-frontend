87abc1cd3bf84e3b2ed83335f69528f2df6523bf
ShinYoung-Kim <lukey0515@naver.com>
2023-09-08 13:24:57 +0900

    [DAD-813] feat: 채널 및 작가 통일 컴포넌트 생성

---
+++ src/components/molcules/CategoryItem/ScrapCard/ChannelInfo.tsx
@@ -0,0 +1,13 @@
+import { contentProps } from "../../../../types/ContentType";
+
+function ChannelInfo(content: contentProps['content']) {
+    const { channelImageUrl, channelName, publishedDate } = content;
+
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default ChannelInfo;
