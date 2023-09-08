d5e56934086221101ca89a2c99dff9716b602c54
ShinYoung-Kim <lukey0515@naver.com>
2023-09-08 14:51:25 +0900

    [DAD-813] feat: 카테고리 스크랩 카드 하나의 스크랩 카드로 통일

--- src/components/organisms/ScrapCard.tsx
+++ src/components/organisms/ScrapCard.tsx
@@ -1,34 +1,32 @@
 import styled from 'styled-components';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { decode } from 'html-entities';
 
-import Tooltip from '../atoms/Tooltip';
 import ScrapEditModal from './ScrapEditModal';
 import ScrapDeleteModal from './ScrapDeleteModal';
 import MemoCreateModal from './MemoCreateModal';
-import OtherScrapCard from '../molcules/OtherScrapCard';
-import ProductScrapCard from '../molcules/ProductScrapCard';
 import VideoScrapCard from '../molcules/VideoScrapCard';
-import ArticleScrapCard from '../molcules/ArticleScrapCard';
 import ErrorHandler from '../../utility/ErrorHandler';
 
 import theme from '../../assets/styles/theme';
 import { contentProps } from '../../types/ContentType';
+import { DescriptionElement } from '@/components/atoms/CategoryItem/DescrptionElement';
+import MemoCreateButton from '@/components/atoms/CategoryItem/MemoCreateButton';
+import { SiteNameElement } from '@/components/atoms/CategoryItem/SiteNameElement';
+import { ThumbnailElement } from '@/components/atoms/CategoryItem/ThumbnailElement';
+import { TitleElement } from '@/components/atoms/CategoryItem/TitleElement';
+import ColumnContainer from '@/components/atoms/ColumnContainer';
+import RowContainer from '@/components/atoms/RowContainer';
+import ChannelInfo from '@/components/molcules/CategoryItem/ScrapCard/ChannelInfo';
+import Memo from '@/components/molcules/Memo';
+import { Box, Typography } from '@mui/material';
+import Tooltip from '@/components/atoms/CategoryItem/Tooltip';
 
 function ScrapCard({ content }: contentProps) {
-    const [isTooltipVisible, setIsTooltipVisible] = useState(false);
     const [isScrapEditModalVisible, setIsScrapEditModalVisible] = useState(false);
     const [isScrapDeleteModalVisible, setIsScrapDeleteModalVisible] = useState(false);
     const [isMemoCreateModalVisible, setIsMemoCreateModalVisible] = useState(false);
     const [error, setError] = useState<string | null>(null);
-
-    function showTooltip() {
-        setIsTooltipVisible(true);
-    }
-
-    function hideTooltip() {
-        setIsTooltipVisible(false);
-    }
 
     function showScrapEditModal() {
         setIsScrapEditModalVisible(true);
@@ -60,27 +58,113 @@ function ScrapCard({ content }: contentProps) {
         description: decode(content.description, { level: 'html5' })
     }
 
-    function matchCardType(dtype: string) {
-        switch (dtype) {
-            case 'other':
-                return <OtherScrapCard content={content} showMemoCreateModal={showMemoCreateModal} showTooltip={showTooltip} />;
-            case 'product':
-                return <ProductScrapCard content={content} showMemoCreateModal={showMemoCreateModal} showTooltip={showTooltip} />;
-            case 'video':
-                return <VideoScrapCard content={content} showMemoCreateModal={showMemoCreateModal} showTooltip={showTooltip} />;
-            case 'article':
-                return <ArticleScrapCard content={content} showMemoCreateModal={showMemoCreateModal} showTooltip={showTooltip} />;
-            default:
-                return <OtherScrapCard content={content} showMemoCreateModal={showMemoCreateModal} showTooltip={showTooltip} />;
-        }
-    }
+    const channelInfoElementArray = [
+        content.channelImageUrl,
+        content.channelName,
+        content.publishedDate,
+        content.author,
+        content.blogName,
+        content.authorImageUrl
+    ];
+    const varient = 'scrapCard';
 
     return (
         <CardContainer>
-            {matchCardType(content.dtype)}
+            <CardWrapper
+                style={{ cursor: 'pointer' }}
+                onClick={(e) => {
+                    e.stopPropagation();
+                    window.open(`${content.pageUrl}`);
+                }}>
+                <Box
+                    component='div'
+                    sx={{
+                        width: '100%',
+                        display: 'flex',
+                        justifyContent: content.siteName ? 'space-between' : 'flex-end',
+                    }}
+                >
+                    {content.siteName && <SiteNameElement siteName={content.siteName} varient={varient} />}
+                    <Tooltip />
+                </Box>
+                {content.title && <TitleElement title={content.title} varient={varient} />}
+                {
+                    channelInfoElementArray.some((element) => !!element) && < ChannelInfo content={content} />
+                }
+                <ThumbnailElement thumbnailUrl={content.thumbnailUrl} />
+                <RowContainer>
+                    {content.watchedCnt &&
+                        <ColumnContainer
+                            style={{
+                                gap: '4px',
+                                flex: '1',
+                            }}
+                        >
+                            <Typography
+                                variant='h6'
+                                color={theme.color.Gray_070}
+                            >
+                                조회수
+                            </Typography>
+                            <Typography
+                                variant='h3'
+                                color={theme.color.Gray_090}
+                                sx={{
+                                    fontWeight: '600',
+                                    lineHeight: '150%',
+                                }}
+                            >
+                                {content.watchedCnt}
+                            </Typography>
+                        </ColumnContainer>
+                    }
+                    {content.playTime &&
+                        <ColumnContainer
+                            style={{
+                                gap: '4px',
+                                flex: '1',
+                            }}
+                        >
+                            <Typography
+                                variant='h6'
+                                color={theme.color.Gray_070}
+                            >
+                                영상 길이
+                            </Typography>
+                            <Typography
+                                variant='h3'
+                                color={theme.color.Gray_090}
+                                sx={{
+                                    fontWeight: '600',
+                                    lineHeight: '150%',
+                                }}
+                            >
+                                {content.playTime}
+                            </Typography>
+                        </ColumnContainer>
+                    }
+                </RowContainer>
+                {content.description && <DescriptionElement description={content.description} varient={varient} />}
+                {content.memoList?.map(memo => {
+                    return <Memo memoImageURL={memo.memoImageUrl} memoText={memo.memoText} />
+                })}
+                <Box
+                    component='div'
+                    onClick={
+                        (e) => {
+                            e.stopPropagation();
+                        }}
+                    sx={{
+                        width: '100%',
+                        display: 'flex',
+                        justifyContent: 'flex-end',
+                    }}
+                >
+                    <MemoCreateButton showMemoCreateModal={showMemoCreateModal} />
+                </Box>
+            </CardWrapper>
             {isScrapEditModalVisible && <ScrapEditModal hideScrapEditModal={hideScrapEditModal} content={content} setError={setError} />}
             {isScrapDeleteModalVisible && <ScrapDeleteModal hideScrapDeleteModal={hideScrapDeleteModal} scrapId={content.scrapId} />}
-            {(isScrapEditModalVisible || isScrapDeleteModalVisible || isMemoCreateModalVisible) && <Overlay />}
             {isMemoCreateModalVisible && <MemoCreateModal hideMemoCreateModal={hideMemoCreateModal} scrapId={content.scrapId} setError={setError} />}
             {error && <ErrorHandler error={error} setError={setError} />}
         </CardContainer>
@@ -95,14 +179,14 @@ const CardContainer = styled.div`
     box-shadow: 0px 2px 16px 0px rgba(19, 48, 74, 0.08);
 `
 
-const Overlay = styled.div`
-    width: 100vw;
-    height: 100vh;
-    position: fixed;
-    top: 0;
-    left: 0;
-    z-index: 1;
-    background-color: rgba(0, 0, 0, 0.5);
+const CardWrapper = styled.div`
+    padding: 15px;
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
+    background: ${theme.color.Gray_020};
+    border-radius: 8px;
+    box-shadow: 0px 2px 16px 0px rgba(19, 48, 74, 0.08);  
 `
 
 export default ScrapCard;
