3ddbfa110abc28b87605d6294dbfe9e53c0b0bac
ShinYoung-Kim <lukey0515@naver.com>
2023-09-08 14:01:42 +0900

    [DAD-813] feat: 채널 정보 컴포넌트 각 엘리먼트 부재 예상하여 구성

--- src/components/molcules/CategoryItem/ScrapCard/ChannelInfo.tsx
+++ src/components/molcules/CategoryItem/ScrapCard/ChannelInfo.tsx
@@ -1,12 +1,72 @@
-import { contentProps } from "../../../../types/ContentType";
+import theme from "@/assets/styles/theme";
+import { ChannelElement } from "@/components/atoms/CategoryItem/ChannelElement";
+import { PublishedDateElement } from "@/components/atoms/CategoryItem/PublishedDateElement";
+import ProfileImage from "@/components/atoms/ProfileImage";
+import { contentProps } from "@/types/ContentType";
+import { Box, Typography } from "@mui/material";
+import { JSX } from "react/jsx-runtime";
 
-function ChannelInfo(content: contentProps['content']) {
-    const { channelImageUrl, channelName, publishedDate } = content;
+function ChannelInfo({ content }: { content: contentProps['content'] }) {
+    const { channelImageUrl, channelName, publishedDate, author, blogName, authorImageUrl } = content;
+    const varient = 'scrapCard';
+    function ChannelInfoImage() {
+        return (
+            <>
+                {channelImageUrl && <ProfileImage source={channelImageUrl} size={20} />}
+                {authorImageUrl && <ProfileImage source={authorImageUrl} size={20} />}
+            </>
+        )
+    }
+
+    function dividerDot() {
+        return (
+            <Typography
+                color={theme.color.Gray_080}
+                variant="h6"
+                sx={{
+                    fontWeight: '300',
+                    lineHeight: '160%',
+                }}
+            >
+                {' • '}
+            </Typography>
+        );
+    }
+
+    function ChannelInfoText() {
+        const channelInfoTextArray: JSX.Element[] = [];
+
+        const channelInfoTextElementTypeArray = [channelName, author, blogName, publishedDate];
+        channelInfoTextElementTypeArray.map((element) => {
+            if (element) {
+                channelInfoTextArray.push(element === publishedDate ? <PublishedDateElement publishedDate={element} varient={varient} /> : <ChannelElement channelName={element} varient={varient} />);
+            }
+        });
+
+        return (
+            <>
+                {channelInfoTextArray.map((element, index) => {
+                    return (
+                        <>
+                            {element}
+                            {index !== channelInfoTextArray.length - 1 && dividerDot()}
+                        </>
+                    )
+                })}
+            </>
+        )
+    }
 
     return (
-        <div>
-
-        </div>
+        <Box
+            sx={{
+                display: 'flex',
+                gap: '8px',
+            }}
+        >
+            <ChannelInfoImage />
+            <ChannelInfoText />
+        </Box>
     );
 }
 
