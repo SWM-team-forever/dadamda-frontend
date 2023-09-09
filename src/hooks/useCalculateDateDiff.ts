511a87cc4313d7607b45ef9c58ced903ccd1cc08
ShinYoung-Kim <lukey0515@naver.com>
2023-09-09 13:34:37 +0900

    [DAD-816] feat: 날짜 차이 계산하는 함수 로직 변경

--- src/hooks/useCalculateDateDiff.ts
+++ src/hooks/useCalculateDateDiff.ts
@@ -3,39 +3,16 @@ import duration, {Duration} from 'dayjs/plugin/duration';
 import utc from 'dayjs/plugin/utc';
 import timezone from 'dayjs/plugin/timezone';
 
+import relativeTime from "dayjs/plugin/relativeTime";
+import "dayjs/locale/ko"; 
+
 dayjs.extend(duration);
 dayjs.extend(utc);
 dayjs.extend(timezone);
+dayjs.extend(relativeTime);
+dayjs.locale("ko");
 
-function changeUnixTimeToDate(publishedDate: number) {
-    const unixTime = dayjs.unix(publishedDate);
-    const publishedDateInDate = unixTime.tz().format('YYYY-MM-DD HH:mm:ss Z');
-    return publishedDateInDate;
-}
 
 export function getTimeDiff(publishedDate: number): string {
-    const publishedDateInDate = changeUnixTimeToDate(publishedDate);
-    const timeDiffDuration: Duration = dayjs.duration(dayjs().diff(publishedDateInDate));
-    const yearDiff: number = parseInt(timeDiffDuration.format('Y'));
-    const monthDiff: number = parseInt(timeDiffDuration.format('M'));
-    const dateDiff: number = parseInt(timeDiffDuration.format('D'));
-    const hourDiff: number = parseInt(timeDiffDuration.format('H'));
-    const minuteDiff: number = parseInt(timeDiffDuration.format('m'));
-    const secondDiff: number = parseInt(timeDiffDuration.format('s'));
-
-    if (yearDiff > 0) {
-        return `${yearDiff}년 전`
-    } else if (monthDiff > 0) {
-        return `${monthDiff}달 전`
-    } else if (dateDiff > 0) {
-        return `${dateDiff}일 전`
-    } else if (hourDiff > 0) {
-        return `${hourDiff}시간 전`
-    } else if (minuteDiff > 0) {
-        return `${minuteDiff}분 전`
-    } else if (secondDiff > 0) {
-        return `${secondDiff}초 전`
-    } else {
-        return ''
-    }
+    return dayjs.unix(publishedDate).fromNow();
 }
