0c8787398ed6ffafe6c9650fdeccce0bc0f54c76
ShinYoung-Kim <lukey0515@naver.com>
2023-09-11 16:54:04 +0900

    [DAD-791] feat: 메모 추가하기 모달 wrapper 디자인 적용

--- src/components/molcules/Modal/MemoCreateModalWrapper.tsx
+++ src/components/molcules/Modal/MemoCreateModalWrapper.tsx
@@ -1,6 +1,8 @@
-import { Modal } from '@mui/material';
+import { Box, Button, Modal, Typography } from '@mui/material';
 
 import { useModal } from '@/hooks/useModal';
+import theme from '@/assets/styles/theme';
+import { CloseIcon } from '@/components/atoms/Icon';
 
 function MemoCreateModalWrapper() {
     const { modal, closeModal } = useModal();
@@ -12,7 +14,68 @@ function MemoCreateModalWrapper() {
             aria-labelledby="parent-modal-title"
             aria-describedby="parent-modal-description"
         >
-            {modal.element}
+            <Box
+                sx={{
+                    backgroundColor: theme.color.Gray_020,
+                    borderRadius: '8px',
+                    p: '32px',
+                    position: 'absolute',
+                    top: '50%',
+                    left: '50%',
+                    transform: 'translate(-50%, -50%)',
+                    width: {
+                        xs: '360px',
+                        md: '435px',
+                    },
+                    display: 'flex',
+                    flexDirection: 'column',
+                    gap: '16px',
+                    boxSizing: 'border-box',
+                }}
+            >
+                <Box
+                    sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                    }}
+                >
+                    <Box
+                        sx={{
+                            alignSelf: 'flex-end',
+                        }}
+                    >
+                        <CloseIcon width='24' height='24' fill={theme.color.Gray_070} />
+                    </Box>
+                    <Typography
+                        variant='h1'
+                        sx={{
+                            fontWeight: '600',
+                            lineHeight: '160%',
+                            alignSelf: 'center',
+                        }}
+                        color={theme.color.Gray_090}
+                    >
+                        {modal.title}
+                    </Typography>
+                </Box>
+                {modal.element}
+                <Button
+                    variant='contained'
+                    sx={{
+                        backgroundColor: theme.color.Gray_050,
+                        borderRadius: '4px',
+                        boxShadow: 'none',
+                        width: 'fit-content',
+                        alignSelf: 'flex-end',
+                        '&:hover': {
+                            backgroundColor: theme.color.Blue_080,
+                            boxShadow: 'none',
+                        }
+                    }}
+                >
+                    등록
+                </Button>
+            </Box>
         </Modal>
     );
 }
