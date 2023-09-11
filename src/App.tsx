9c3843ff81295a40bc5f023a255c313482f53b4e
ShinYoung-Kim <lukey0515@naver.com>
2023-09-11 16:11:53 +0900

    [DAD-791] feat: 모달 위치 최상단으로 지정

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,30 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
-import './App.css'
-import ScrapPage from './pages/ScrapPage';
-import Header from './components/molcules/Navigation/Header.tsx';
-import MainPage from './pages/MainPage';
-import UserPage from './pages/UserPage';
-import TrendingPage from './pages/TrendingPage';
-import BoardPage from './pages/BoardPage';
-import GoogleOAuthLoginpage from './pages/GoogleOAuthLoginPage.tsx';
-import { LoginProvider, RequireAuth } from './context/LoginContext.tsx';
-import ScrapTemplate from './components/templates/ScrapTemplate.tsx';
-import PrivacyPolicyPage from './pages/PrivacyPolicyPage.tsx';
-import { SnackbarProvider } from 'notistack';
-import NotReadyTemplate from './components/templates/NotReadyTemplate.tsx';
-import { worker } from './mocks/worker.ts';
-import ErrorHandler from './utility/ErrorHandler.tsx';
-import { useState } from 'react';
-import { ThemeProvider } from '@mui/material';
-import theme from './assets/styles/themeMuiStyle.ts';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { SnackbarProvider } from 'notistack';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 
-// if (process.env.NODE_ENV === 'development') {
-//   // develop 환경에서만 사용
-//   worker.start();
-// }
+import theme from '@/assets/styles/theme';
+import { LoginProvider, RequireAuth } from '@/context/LoginContext';
+import { useModal } from '@/hooks/useModal';
+
+import MemoCreateModalWrapper from '@/components/molcules/Modal/MemoCreateModalWrapper';
+import Header from '@/components/molcules/Navigation/Header';
+import NotReadyTemplate from '@/components/templates/NotReadyTemplate';
+import ScrapTemplate from '@/components/templates/ScrapTemplate';
+import BoardPage from '@/pages/BoardPage';
+import GoogleOAuthLoginpage from '@/pages/GoogleOAuthLoginPage';
+import MainPage from '@/pages/MainPage';
+import PrivacyPolicyPage from '@/pages/PrivacyPolicyPage';
+import ScrapPage from '@/pages/ScrapPage';
+import TrendingPage from '@/pages/TrendingPage';
+import UserPage from '@/pages/UserPage';
 
 const queryClient = new QueryClient();
 
 function App() {
-  const [error, setError] = useState<string | null>(null);
+  const { modal } = useModal();
+
   return (
     <>
       <QueryClientProvider client={queryClient}>
@@ -38,11 +33,11 @@ function App() {
             <LoginProvider>
               <BrowserRouter>
                 <Header />
-                {error && <ErrorHandler error={error} setError={setError} />}
+                {modal.isOpen && <MemoCreateModalWrapper />}
                 <Routes>
                   <Route path='/' element={<MainPage />}></Route>
                   <Route path='/main' element={<MainPage />}></Route>
-                  <Route path='/user' element={<RequireAuth><UserPage setError={setError} /></RequireAuth>}></Route>
+                  <Route path='/user' element={<RequireAuth><UserPage /></RequireAuth>}></Route>
                   <Route path='/scrap' element={<RequireAuth><ScrapPage /></RequireAuth>}>
                     <Route path='list' element={<ScrapTemplate type={'list'} />}></Route>
                     <Route path='article' element={<ScrapTemplate type={'article'} />}></Route>
@@ -56,7 +51,7 @@ function App() {
                     <Route index element={<NotReadyTemplate />} />
                   </Route>
                   <Route path='/trending' element={<TrendingPage />}></Route>
-                  <Route path='/google-login' element={<GoogleOAuthLoginpage setError={setError} />}></Route>
+                  <Route path='/google-login' element={<GoogleOAuthLoginpage />}></Route>
                   <Route path='/privacy' element={<PrivacyPolicyPage />}></Route>
                 </Routes>
               </BrowserRouter>
