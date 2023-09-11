ddf0382837b3642c0a8dd0ce0a84102685878609
ShinYoung-Kim <lukey0515@naver.com>
2023-09-11 19:24:00 +0900

    [DAD-791] feat: 메모 추가하기 모달 버튼 기능 연결

--- src/components/atoms/Modal/MemoCreateModalElement.tsx
+++ src/components/atoms/Modal/MemoCreateModalElement.tsx
@@ -1,18 +1,41 @@
-import { Box, OutlinedInput, Typography } from '@mui/material';
+import { Box, Button, CircularProgress, OutlinedInput, Typography } from '@mui/material';
 import { useState, useEffect, ChangeEvent } from 'react';
 
 import theme from '@/assets/styles/theme';
+import { useModal } from '@/hooks/useModal';
+import { usePostCreateMemo } from '@/api/memo';
+import { useDefaultSnackbar } from '@/hooks/useWarningSnackbar';
 
 function MemoCreateModalElement() {
-    const [textAreaValue, setTextAreaValue] = useState('');
     const [, setToken] = useState<string | null>(null);
     useEffect(() => {
         setToken(localStorage.getItem('token'));
     }, []);
 
+    const { modal, closeModal } = useModal();
+    const [textAreaValue, setTextAreaValue] = useState('');
+
     const handleSetValue = (e: ChangeEvent<HTMLTextAreaElement>) => {
         e.preventDefault();
         setTextAreaValue(e.target.value);
+    }
+
+    const scrapId = modal.scrapId;
+    const token = localStorage.getItem('token');
+    const { mutate, isLoading, isError } = usePostCreateMemo();
+
+    if (isLoading) {
+        return <CircularProgress
+            sx={{
+                position: 'absolute',
+                top: '50%',
+                left: '50%',
+                transform: 'translate(-50%, -50%)',
+            }} />;
+    }
+
+    if (isError) {
+        useDefaultSnackbar('메모 생성에 실패하였습니다.', 'error');
     }
 
     const MAX_MEMO_LENGTH = 1000;
@@ -45,6 +68,28 @@ function MemoCreateModalElement() {
             >
                 {textAreaValue.length} / {MAX_MEMO_LENGTH}자
             </Typography>
+            <Button
+                variant='contained'
+                sx={{
+                    backgroundColor: theme.color.Gray_050,
+                    borderRadius: '4px',
+                    boxShadow: 'none',
+                    width: 'fit-content',
+                    alignSelf: 'flex-end',
+                    '&:hover': {
+                        backgroundColor: theme.color.Blue_080,
+                        boxShadow: 'none',
+                    }
+                }}
+                onClick={
+                    () => {
+                        (token && scrapId) && mutate({ token, scrapId, textAreaValue });
+                        closeModal();
+                    }
+                }
+            >
+                등록
+            </Button>
         </Box>
     );
 }
