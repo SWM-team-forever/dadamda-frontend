323a7890e9faedf80103acb140e1a5eb13a07af4
ShinYoung-Kim <lukey0515@naver.com>
2023-09-12 19:01:51 +0900

    [DAD-804] refactor: 불필요한 코드 제거

--- src/components/atoms/Modal/scrapDeleteModal.test.tsx
+++ src/components/atoms/Modal/scrapDeleteModal.test.tsx
@@ -1,11 +1,7 @@
-import { act, render, renderHook } from '@testing-library/react';
+import { act, renderHook } from '@testing-library/react';
 import { expect, describe, it } from 'vitest';
 
 import { useModal } from '@/hooks/useModal';
-import ScrapCard from '@/components/organisms/ScrapCard';
-import { userEvent } from '@storybook/testing-library';
-import scrapCardDataMock from '__mocks__/scrapCardDataMock';
-import ModalWrapper from '@/components/molcules/Modal/ModalWrapper';
 
 describe('스크랩 삭제하기 모달 테스트', () => {
     it('스크랩 삭제 옵션에 대한 openModal 함수가 잘 동작한다.', () => {
