d7aaa965c9ec5a3dd28c826cd62c301e0cffb3c3
ShinYoung-Kim <lukey0515@naver.com>
2023-09-12 17:13:06 +0900

    [DAD-804] style: 스크랩 삭제 모달 스타일 적용

--- src/components/atoms/Modal/ScrapDeleteModalElement.tsx
+++ src/components/atoms/Modal/ScrapDeleteModalElement.tsx
@@ -1,7 +1,21 @@
 import { useDeleteScrap } from '@/api/scrap';
+import theme from '@/assets/styles/theme';
 import { useDefaultSnackbar } from '@/hooks/useWarningSnackbar';
-import { CircularProgress, Box } from '@mui/material';
+import { CircularProgress, Box, Typography, Button } from '@mui/material';
 import { useState, useEffect } from 'react';
+
+const scrapDeleteModalButtonStyle = {
+    backgroundColor: theme.color.Gray_050,
+    borderRadius: '4px',
+    boxShadow: 'none',
+    width: 'fit-content',
+    alignSelf: 'flex-end',
+    p: '5px',
+    '&:hover': {
+        backgroundColor: theme.color.Blue_080,
+        boxShadow: 'none',
+    }
+}
 
 function ScrapDeleteElementModal() {
     const [token, setToken] = useState<string | null>(null);
@@ -12,8 +26,43 @@ function ScrapDeleteElementModal() {
     const { mutate } = useDeleteScrap();
 
     return (
-        <Box>
-
+        <Box
+            sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                gap: '10px',
+            }}
+        >
+            <Typography
+                variant='h4'
+                color={theme.color.Gray_080}
+                sx={{
+                    fontWeight: '500',
+                    alignSelf: 'center',
+                }}
+            >
+                한 번 삭제된 스크랩은 다시 복구할 수 없습니다. 정말 삭제하시겠습니까?
+            </Typography>
+            <Box
+                sx={{
+                    display: 'flex',
+                    gap: '5px',
+                    alignSelf: 'flex-end',
+                }}
+            >
+                <Button
+                    variant='contained'
+                    sx={scrapDeleteModalButtonStyle}
+                >
+                    취소하기
+                </Button>
+                <Button
+                    variant='contained'
+                    sx={scrapDeleteModalButtonStyle}
+                >
+                    삭제하기
+                </Button>
+            </Box>
         </Box>
     );
 }
