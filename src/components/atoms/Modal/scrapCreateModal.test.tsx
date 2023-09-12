dbd01d27793464464646d45bd9c52200dd4356cc
ShinYoung-Kim <lukey0515@naver.com>
2023-09-12 14:05:39 +0900

    [DAD-803] test: 스크랩 추가 버튼과 스크랩 생성 모달 연결 확인 테스트 작업

---
+++ src/components/atoms/Modal/scrapCreateModal.test.tsx
@@ -0,0 +1,25 @@
+import { act, render, renderHook } from '@testing-library/react';
+import { expect, describe, it } from 'vitest';
+import { screen } from '@testing-library/dom';
+import userEvent from '@testing-library/user-event';
+import scrapCardDataMock from '__mocks__/scrapCardDataMock';
+
+import { useModal } from '@/hooks/useModal';
+import ScrapListHeader from '@/components/molcules/ScrapListHeader';
+
+describe('스크랩 추가하기 모달 테스트', () => {
+    it('메모 추가하기 버튼을 누르면 모달 atom의 isOpen 상태가 변경된다.', () => {
+        const { result } = renderHook(() => useModal());
+        expect(result.current.modal.isOpen).toBe(false);
+
+        act(() => {
+            render(<ScrapListHeader count={1} type={'list'} />)
+        });
+
+        act(() => {
+            userEvent.click(screen.getByText('+ 스크랩 추가'));
+        });
+
+        expect(result.current.modal.isOpen).toBe(true);
+    });
+})
