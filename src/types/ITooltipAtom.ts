ecfa2e73f368f14f1b8c7d0dde578a26d2374a7f
ShinYoung-Kim <lukey0515@naver.com>
2023-09-12 15:17:39 +0900

    [DAD-820] feat: 툴팁 atom interface 추가

---
+++ src/types/ITooltipAtom.ts
@@ -0,0 +1,9 @@
+interface ITooltipAtom {
+    isOpen: boolean,
+    menu: {
+        title: string,
+        clickAction: (e: React.MouseEvent<HTMLElement>) => void,
+    }
+}
+
+export type {ITooltipAtom};
