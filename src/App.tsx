95b57bb2238a27d163b2792537e44653f5118301
ShinYoung-Kim <lukey0515@naver.com>
2023-09-14 10:49:47 +0900

    [DAD-809] feat: 에러 바운더리, 에러 페이지 연결

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { SnackbarProvider } from 'notistack';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from '@mui/material';
+import { ErrorBoundary } from 'react-error-boundary';
 
 import theme from '@/assets/styles/themeMuiStyle';
 import { LoginProvider, RequireAuth } from '@/context/LoginContext';
@@ -19,6 +20,7 @@ import PrivacyPolicyPage from '@/pages/PrivacyPolicyPage';
 import ScrapPage from '@/pages/ScrapPage';
 import TrendingPage from '@/pages/TrendingPage';
 import UserPage from '@/pages/UserPage';
+import ErrorPage from '@/pages/ErrorPage';
 
 const queryClient = new QueryClient();
 
@@ -34,26 +36,33 @@ function App() {
               <BrowserRouter>
                 <Header />
                 {modal.isOpen && <ModalWrapper />}
-                <Routes>
-                  <Route path='/' element={<MainPage />}></Route>
-                  <Route path='/main' element={<MainPage />}></Route>
-                  <Route path='/user' element={<RequireAuth><UserPage /></RequireAuth>}></Route>
-                  <Route path='/scrap' element={<RequireAuth><ScrapPage /></RequireAuth>}>
-                    <Route path='list' element={<ScrapTemplate type={'list'} />}></Route>
-                    <Route path='article' element={<ScrapTemplate type={'article'} />}></Route>
-                    <Route path='product' element={<ScrapTemplate type={'product'} />}></Route>
-                    <Route path='video' element={<ScrapTemplate type={'video'} />}></Route>
-                    <Route path='location' element={<ScrapTemplate type={'location'} />}></Route>
-                    <Route path='other' element={<ScrapTemplate type={'other'} />}></Route>
-                    <Route index element={<ScrapTemplate type={'list'} />}></Route>
-                  </Route>
-                  <Route path='/board' element={<RequireAuth><BoardPage /></RequireAuth>}>
-                    <Route index element={<NotReadyTemplate />} />
-                  </Route>
-                  <Route path='/trending' element={<TrendingPage />}></Route>
-                  <Route path='/google-login' element={<GoogleOAuthLoginpage />}></Route>
-                  <Route path='/privacy' element={<PrivacyPolicyPage />}></Route>
-                </Routes>
+                <ErrorBoundary
+                  FallbackComponent={ErrorPage}
+                  onReset={() => {
+                    window.location.reload();
+                  }}
+                >
+                  <Routes>
+                    <Route path='/' element={<MainPage />}></Route>
+                    <Route path='/main' element={<MainPage />}></Route>
+                    <Route path='/user' element={<RequireAuth><UserPage /></RequireAuth>}></Route>
+                    <Route path='/scrap' element={<RequireAuth><ScrapPage /></RequireAuth>}>
+                      <Route path='list' element={<ScrapTemplate type={'list'} />}></Route>
+                      <Route path='article' element={<ScrapTemplate type={'article'} />}></Route>
+                      <Route path='product' element={<ScrapTemplate type={'product'} />}></Route>
+                      <Route path='video' element={<ScrapTemplate type={'video'} />}></Route>
+                      <Route path='location' element={<ScrapTemplate type={'location'} />}></Route>
+                      <Route path='other' element={<ScrapTemplate type={'other'} />}></Route>
+                      <Route index element={<ScrapTemplate type={'list'} />}></Route>
+                    </Route>
+                    <Route path='/board' element={<RequireAuth><BoardPage /></RequireAuth>}>
+                      <Route index element={<NotReadyTemplate />} />
+                    </Route>
+                    <Route path='/trending' element={<TrendingPage />}></Route>
+                    <Route path='/google-login' element={<GoogleOAuthLoginpage />}></Route>
+                    <Route path='/privacy' element={<PrivacyPolicyPage />}></Route>
+                  </Routes>
+                </ErrorBoundary>
               </BrowserRouter>
               <ReactQueryDevtools initialIsOpen={false} />
             </LoginProvider>
