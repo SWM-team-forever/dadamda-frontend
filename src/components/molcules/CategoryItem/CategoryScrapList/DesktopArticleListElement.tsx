45a73353b1dcdbdbee5dc6cc9d25a771241dcaf1
ShinYoung-Kim <lukey0515@naver.com>
2023-09-17 14:34:35 +0900

    [DAD-789] feat: 아티클 스크랩 용 list 요소 생성

--- src/components/molcules/CategoryItem/CategoryScrapList/DesktopArticleListElement.tsx
+++ src/components/molcules/CategoryItem/CategoryScrapList/DesktopArticleListElement.tsx
@@ -1,83 +1,168 @@
-import { CardActionArea, Box, CardContent } from "@mui/material";
+import styled from 'styled-components';
+import { useState } from 'react';
+import { decode } from 'html-entities';
+import { Box, Typography } from '@mui/material';
 
-import theme from "@/assets/styles/theme";
-import { useCategoryItemSelected } from "@/context/CategoryItemContext";
-import { contentProps } from "@/types/ContentType";
+import theme from '@/assets/styles/theme';
+import { contentProps } from '@/types/ContentType';
+import { useModal } from '@/hooks/useModal';
+import { useTooltip } from '@/hooks/useTooltip';
 
-import { AuthorElement } from "@/components/atoms/CategoryItem/AuthorElement";
-import { AuthorImageElement } from "@/components/atoms/CategoryItem/AuthorImageElement";
-import { BlogNameElement } from "@/components/atoms/CategoryItem/BlogNameElement";
-import { PublishedDateElement } from "@/components/atoms/CategoryItem/PublishedDateElement";
-import { SiteNameElement } from "@/components/atoms/CategoryItem/SiteNameElement";
-import { ThumbnailElement } from "@/components/atoms/CategoryItem/ThumbnailElement";
-import { TitleElement } from "@/components/atoms/CategoryItem/TitleElement";
-import ColumnContainer from "@/components/atoms/ColumnContainer";
-import RowContainer from "@/components/atoms/RowContainer";
+import { DescriptionElement } from '@/components/atoms/CategoryItem/DescrptionElement';
+import MemoCreateButton from '@/components/atoms/CategoryItem/MemoCreateButton';
+import { SiteNameElement } from '@/components/atoms/CategoryItem/SiteNameElement';
+import { ThumbnailElement } from '@/components/atoms/CategoryItem/ThumbnailElement';
+import { TitleElement } from '@/components/atoms/CategoryItem/TitleElement';
+import ColumnContainer from '@/components/atoms/ColumnContainer';
+import RowContainer from '@/components/atoms/RowContainer';
+import ChannelInfo from '@/components/molcules/CategoryItem/ScrapCard/ChannelInfo';
+import Memo from '@/components/molcules/Memo';
+import ScrapEditModal from '@/components/organisms/ScrapEditModal';
+import TooltipWrapper from '@/components/atoms/CategoryItem/TooltipWrapper';
+import { useSelectedScrap } from '@/hooks/useSelectedScrap';
 
-interface DesktopArticleElementProps {
-    content: contentProps['content'],
-}
+function DesktopArticleListElement({ content }: contentProps) {
+    content = {
+        ...content,
+        title: decode(content.title, { level: 'html5' }),
+        description: decode(content.description, { level: 'html5' })
+    }
 
-function DesktopArticleListElement({ content }: DesktopArticleElementProps) {
-    const { thumbnailUrl, blogName, title, author, authorImageUrl, siteName, publishedDate } = content;
-    const [selectedContent, setSelectedContent] = useCategoryItemSelected();
+    const channelInfoElementArray = [
+        content.channelImageUrl,
+        content.channelName,
+        content.publishedDate,
+        content.author,
+        content.blogName,
+        content.authorImageUrl
+    ];
+    const varient = 'scrapCard';
 
-    const varient = 'desktopArticleList';
+    const { openModal, connectMemoWithScrapId } = useModal();
+    const { closeTooltip } = useTooltip();
+    const { selectedScrap, setSelectedScrap } = useSelectedScrap();
+
+    const menuItemContentList = [
+        {
+            title: '스크랩 수정',
+            clickAction: (e: React.MouseEvent<HTMLElement>) => {
+                setSelectedScrap(content);
+                openModal('scrapEdit');
+                closeTooltip(e);
+            }
+        },
+        {
+            title: '스크랩 삭제',
+            clickAction: (e: React.MouseEvent<HTMLElement>) => {
+                openModal('scrapDelete');
+                closeTooltip(e);
+            }
+        }
+    ]
+
+    function isSelectedScrap() {
+        return content.scrapId === selectedScrap.scrapId;
+    }
 
     return (
-        <div
-            style={{
-                width: '100%',
-                boxShadow: theme.style.shadow,
-                borderRadius: '4px',
-                display: 'block',
-                backgroundColor: `${selectedContent.scrapId === content.scrapId ? theme.color.background_color : 'white'}`,
+        <Box
+            sx={{
+                position: 'relative',
+                wordBreak: 'break-all',
+                borderRadius: '8px',
+                backgroundColor: theme.color.Gray_020,
+                boxShadow: '0px 2px 16px 0px rgba(19, 48, 74, 0.08)',
+                border: isSelectedScrap() ? `2px solid ${theme.color.Blue_070}` : 'none',
             }}
-            onClick={() => setSelectedContent(content)}
         >
-            <CardActionArea
-                sx={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    width: '100%',
-                    gap: '5px',
-                    padding: '10px',
-                }}
-            >
-                <Box sx={{
-                    width: '100%',
+            <CardWrapper
+                style={{ cursor: 'pointer' }}
+                onClick={(e) => {
+                    e.stopPropagation();
+                    setSelectedScrap(content);
                 }}>
-                    <ThumbnailElement thumbnailUrl={thumbnailUrl} />
+                <Box
+                    component='div'
+                    sx={{
+                        width: '100%',
+                        display: 'flex',
+                        justifyContent: content.siteName ? 'space-between' : 'flex-end',
+                    }}
+                >
+                    {content.siteName && <SiteNameElement siteName={content.siteName} varient={varient} />}
+                    <TooltipWrapper menu={menuItemContentList} scrapId={content.scrapId} />
                 </Box>
-                <CardContent sx={{
-                    width: '100%',
-                    padding: '0',
-                    display: 'flex',
-                    flexDirection: 'column',
-                    gap: '5px',
-                }}>
-                    <SiteNameElement siteName={siteName} varient={varient} />
-                    <TitleElement title={title} varient={varient} />
-                    <RowContainer
-                        style={{
-                            justifyContent: 'space-between',
-                        }}>
-                        <RowContainer
+                {content.title && <TitleElement title={content.title} varient={varient} />}
+                {
+                    channelInfoElementArray.some((element) => !!element) && < ChannelInfo content={content} />
+                }
+                <ThumbnailElement thumbnailUrl={content.thumbnailUrl} />
+                <RowContainer>
+                    {content.watchedCnt &&
+                        <ColumnContainer
                             style={{
-                                gap: '5px',
-                            }}>
-                            <AuthorImageElement authorImage={authorImageUrl} />
-                            <ColumnContainer>
-                                <AuthorElement author={author} varient={varient} />
-                                <BlogNameElement blogName={blogName} varient={varient} />
-                            </ColumnContainer>
-                        </RowContainer>
-                        <PublishedDateElement publishedDate={publishedDate} varient={varient} />
-                    </RowContainer>
-                </CardContent>
-            </CardActionArea>
-        </div>
+                                gap: '4px',
+                                flex: '1',
+                            }}
+                        >
+                            <Typography
+                                variant='h6'
+                                color={theme.color.Gray_070}
+                            >
+                                조회수
+                            </Typography>
+                            <Typography
+                                variant='h3'
+                                color={theme.color.Gray_090}
+                                sx={{
+                                    fontWeight: '600',
+                                    lineHeight: '150%',
+                                }}
+                            >
+                                {content.watchedCnt}
+                            </Typography>
+                        </ColumnContainer>
+                    }
+                    {content.playTime &&
+                        <ColumnContainer
+                            style={{
+                                gap: '4px',
+                                flex: '1',
+                            }}
+                        >
+                            <Typography
+                                variant='h6'
+                                color={theme.color.Gray_070}
+                            >
+                                영상 길이
+                            </Typography>
+                            <Typography
+                                variant='h3'
+                                color={theme.color.Gray_090}
+                                sx={{
+                                    fontWeight: '600',
+                                    lineHeight: '150%',
+                                }}
+                            >
+                                {content.playTime}
+                            </Typography>
+                        </ColumnContainer>
+                    }
+                </RowContainer>
+                {content.description && <DescriptionElement description={content.description} varient={varient} />}
+            </CardWrapper>
+        </Box>
     )
 }
+
+const CardWrapper = styled.div`
+    padding: 15px;
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
+    background: ${theme.color.Gray_020};
+    border-radius: 8px;
+    box-shadow: 0px 2px 16px 0px rgba(19, 48, 74, 0.08);  
+`
 
 export default DesktopArticleListElement;
