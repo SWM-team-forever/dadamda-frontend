11ab0eeaa48451887c6cd4dcae2f2b35afa7d0a6
ShinYoung-Kim <lukey0515@naver.com>
2023-09-19 20:25:57 +0900

    [DAD-853] feat: 보드 리스트 템플릿 가짜 구현

--- src/components/templates/BoardListTemplate.tsx
+++ src/components/templates/BoardListTemplate.tsx
@@ -12,50 +12,56 @@ import EmptyScrapContainer from '@/components/organisms/EmptyScrapContainer';
 import MatchTemplateWithTypeAndCount from '@/components/templates/MatchTemplateWithTypeAndCount';
 import { useQuery } from '@tanstack/react-query';
 import { useGetScrapCount } from '@/api/count';
+import BoardListHeader from '@/components/molcules/BoardListHeader';
+import theme from '@/assets/styles/theme';
 
-interface ScrapTemplateProps {
-    type: string,
-}
-
-function BoardListTemplate({ type }: ScrapTemplateProps) {
+function BoardListTemplate() {
     const token = localStorage.getItem('token');
-    const providingTemplates = ['other', 'list', 'video', 'product', 'article'];
 
-    const { data, isLoading, isFetched } = useQuery(['scrapCount', type],
-        () => {
-            return providingTemplates.includes(type)
-                ? (token && useGetScrapCount({ type: type, token: token }))
-                : { data: { count: 0 } };
-        },
-        {
-            enabled: !!token,
-            refetchOnWindowFocus: false,
-            select: (data) => {
-                return data?.data.count;
-            }
-        }
-    );
+    // const { data, isLoading, isFetched } = useQuery(['boardCount'],
+    //     () => {
+    //         return providingTemplates.includes(type)
+    //             ? (token && useGetScrapCount({ type: type, token: token }))
+    //             : { data: { count: 0 } };
+    //     },
+    //     {
+    //         enabled: !!token,
+    //         refetchOnWindowFocus: false,
+    //         select: (data) => {
+    //             return data?.data.count;
+    //         }
+    //     }
+    // );
 
-    if (isLoading) {
-        return <CircularProgress
-            sx={{
-                position: 'absolute',
-                top: '50%',
-                left: '50%',
-                transform: 'translate(-50%, -50%)',
-            }} />;
-    }
+    // if (isLoading) {
+    //     return <CircularProgress
+    //         sx={{
+    //             position: 'absolute',
+    //             top: '50%',
+    //             left: '50%',
+    //             transform: 'translate(-50%, -50%)',
+    //         }} />;
+    // }
 
     return (
         <>
             <ScrapListContainer>
-                {isFetched && <ScrapListHeader type={type} count={data} />}
+                <BoardListHeader count={1} />
                 <Box
                     sx={{
                         height: 'calc(100% - 145px)',
                     }}
                 >
-                    {isFetched && <MatchTemplateWithTypeAndCount type={type} count={data} />}
+                    {/* <MatchTemplateWithTypeAndCount type={type} count={data} /> */}
+                    <Box
+                        sx={{
+                            width: '320px',
+                            height: '180px',
+                            backgroundColor: theme.color.Blue_090,
+                        }}
+                    >
+                        보드 1
+                    </Box>
                 </Box>
             </ScrapListContainer>
         </>
