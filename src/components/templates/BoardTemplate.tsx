0c2c99d145fca2dcf201bd2945be43149a488331
ShinYoung-Kim <lukey0515@naver.com>
2023-09-20 00:41:21 +0900

    [DAD-867] feat: column 버튼 생성

--- src/components/templates/BoardTemplate.tsx
+++ src/components/templates/BoardTemplate.tsx
@@ -1,9 +1,25 @@
+import { Box, Button } from "@mui/material";
+import { useState } from "react";
+
 function BoardTemplate({ boardId }: { boardId: string | null }) {
+    const [columns, setColumns] = useState([]);
+
     return (
         <div>
             보드 {boardId} 보드 페이지
+            <Box>
+                <Button
+                    onClick={createNewColumn}
+                >
+                    + Add Column
+                </Button>
+            </Box>
         </div>
     );
+
+    function createNewColumn() {
+
+    }
 }
 
 export default BoardTemplate;
