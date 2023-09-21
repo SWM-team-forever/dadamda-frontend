a0fbd1393e1d4c20eeb9b1a8328b70462ca3e3c3
ShinYoung-Kim <lukey0515@naver.com>
2023-09-21 12:18:48 +0900

    [DAD-867] feat: taskcard edit mode용 textarea 생성

--- src/components/molcules/Board/TaskCard.tsx
+++ src/components/molcules/Board/TaskCard.tsx
@@ -1,3 +1,4 @@
+import TextArea from "@/components/atoms/TextArea";
 import { Task, id } from "@/components/templates/BoardTemplate";
 import { Box, Button } from "@mui/material";
 import { useState } from "react";
@@ -5,15 +6,42 @@ import { useState } from "react";
 interface Props {
     task: Task;
     deleteTask: (id: id) => void;
+    updateTask: (id: id, content: string) => void;
 }
 
-function TaskCard({ task, deleteTask }: Props) {
+function TaskCard({ task, deleteTask, updateTask }: Props) {
     const [mouseIsOver, setMouseIsOver] = useState(false);
     const [editMode, setEditMode] = useState(false);
 
     const toggleEditMode = () => {
         setEditMode(prev => !prev);
         setMouseIsOver(false);
+    }
+
+    if (editMode) {
+        return (
+            <Box
+                sx={{
+                    cursor: 'grab',
+                }}
+                onClick={toggleEditMode}
+            >
+                <textarea
+                    value={task.content}
+                    autoFocus
+                    placeholder="task content"
+                    onBlur={toggleEditMode}
+                    onKeyDown={(e) => {
+                        if (e.key === 'Enter') {
+                            toggleEditMode();
+                        }
+                    }}
+                    onChange={(e) => {
+                        updateTask(task.id, e.target.value);
+                    }}
+                />
+            </Box>
+        )
     }
 
     return (
