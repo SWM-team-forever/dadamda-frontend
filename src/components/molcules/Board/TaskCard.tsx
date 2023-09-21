c31ee8cf0855ca05934078719df7aedc28f7e01a
ShinYoung-Kim <lukey0515@naver.com>
2023-09-21 11:12:50 +0900

    [DAD-867] feat: hover하면 삭제 버튼 보이기 구현

--- src/components/molcules/Board/TaskCard.tsx
+++ src/components/molcules/Board/TaskCard.tsx
@@ -1,19 +1,24 @@
 import { Task } from "@/components/templates/BoardTemplate";
 import { Box, Button } from "@mui/material";
+import { useState } from "react";
 
 interface Props {
     task: Task;
 }
 
 function TaskCard({ task }: Props) {
+    const [mouseIsOver, setMouseIsOver] = useState(false);
+
     return (
         <Box
             sx={{
                 cursor: 'grab',
             }}
+            onMouseEnter={() => setMouseIsOver(true)}
+            onMouseLeave={() => setMouseIsOver(false)}
         >
             {task.content}
-            <Button>- Delete</Button>
+            {mouseIsOver && <Button>- Delete</Button>}
         </Box>
     );
 }
