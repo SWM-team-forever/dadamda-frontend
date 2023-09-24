4c2b9eafa634403f53b1090fecb711eb5fe73966
ShinYoung-Kim <lukey0515@naver.com>
2023-09-24 16:35:04 +0900

    [DAD-856] style: 우측 패널 모달 모양 잡기

---
+++ src/components/molcules/Modal/RightSideModalWrapper.tsx
@@ -0,0 +1,70 @@
+import { Box, Modal, Typography } from '@mui/material';
+
+import { useModal } from '@/hooks/useModal';
+import theme from '@/assets/styles/theme';
+import { CloseIcon } from '@/components/atoms/Icon';
+
+function RightSideModalWrapper() {
+    const { modal, closeModal } = useModal();
+
+    return (
+        <Modal
+            open={modal.isOpen}
+            onClose={closeModal}
+            aria-labelledby="parent-modal-title"
+            aria-describedby="parent-modal-description"
+        >
+            <Box
+                sx={{
+                    backgroundColor: theme.color.Gray_020,
+                    borderRadius: '8px',
+                    p: '16px',
+                    position: 'absolute',
+                    top: '0',
+                    right: '0',
+                    width: {
+                        xs: '360px',
+                        md: '435px',
+                    },
+                    height: '100%',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    gap: '16px',
+                    boxSizing: 'border-box',
+                }}
+            >
+                <Box
+                    sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                    }}
+                >
+                    <Box
+                        sx={{
+                            alignSelf: 'flex-end',
+                            cursor: 'pointer',
+                        }}
+                        onClick={closeModal}
+                    >
+                        <CloseIcon width='24' height='24' fill={theme.color.Gray_070} />
+                    </Box>
+                    <Typography
+                        variant='h1'
+                        sx={{
+                            fontWeight: '600',
+                            lineHeight: '160%',
+                            alignSelf: 'center',
+                        }}
+                        color={theme.color.Gray_090}
+                    >
+                        {modal.title}
+                    </Typography>
+                </Box>
+                {modal.element}
+            </Box>
+        </Modal>
+    );
+}
+
+export default RightSideModalWrapper;
+
