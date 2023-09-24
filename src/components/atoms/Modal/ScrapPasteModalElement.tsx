3416b1463ad7a8b9aec9877b3378b229fff8d54f
ShinYoung-Kim <lukey0515@naver.com>
2023-09-24 19:15:30 +0900

    [DAD-856] feat: 스크랩 복사 모달 탭 on change 구현

--- src/components/atoms/Modal/ScrapPasteModalElement.tsx
+++ src/components/atoms/Modal/ScrapPasteModalElement.tsx
@@ -1,24 +1,38 @@
 import SearchBar from "@/components/molcules/SearchBar";
+import { TabContext, TabPanel } from "@mui/lab";
 import { Box, Tab, Tabs } from "@mui/material";
+import { useState } from "react";
 
 function ScrapPasteModalElement() {
+    const [value, setValue] = useState('list');
+    const handleTabValueChange = (event: React.SyntheticEvent, newValue: string) => {
+        setValue(newValue);
+    }
+
     return (
         <Box>
-            <SearchBar type={"list"} />
-            <Tabs
-                variant="fullWidth"
-                sx={{
-                    '& .MuiButtonBase-root': {
-                        minWidth: 'auto',
-                    }
-                }}
-            >
-                <Tab label="전체" />
-                <Tab label="상품" />
-                <Tab label="아티클" />
-                <Tab label="비디오" />
-                <Tab label="장소" />
-            </Tabs>
+            <TabContext value={value}>
+
+                <SearchBar type="list" />
+                <Tabs
+                    variant="fullWidth"
+                    sx={{
+                        '& .MuiButtonBase-root': {
+                            minWidth: 'auto',
+                        }
+                    }}
+                    onChange={handleTabValueChange}
+                >
+                    <Tab label="전체" value='list' />
+                    <Tab label="상품" value='product' />
+                    <Tab label="아티클" value='article' />
+                    <Tab label="비디오" value='video' />
+                    <Tab label="장소" value='place' />
+                </Tabs>
+                <TabPanel value={value}>
+                    {value}
+                </TabPanel>
+            </TabContext>
         </Box>
     );
 }
