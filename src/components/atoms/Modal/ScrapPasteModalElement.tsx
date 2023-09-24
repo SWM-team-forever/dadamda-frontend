df62fa6cafb278bc2cfbdfc5fac1a993a50a1ef9
ShinYoung-Kim <lukey0515@naver.com>
2023-09-24 16:54:57 +0900

    [DAD-856] feat: 스크랩 paste 모달에 tab 요소들 추가

--- src/components/atoms/Modal/ScrapPasteModalElement.tsx
+++ src/components/atoms/Modal/ScrapPasteModalElement.tsx
@@ -1,10 +1,24 @@
 import SearchBar from "@/components/molcules/SearchBar";
-import { Box } from "@mui/material";
+import { Box, Tab, Tabs } from "@mui/material";
 
 function ScrapPasteModalElement() {
     return (
         <Box>
             <SearchBar type={"list"} />
+            <Tabs
+                variant="fullWidth"
+                sx={{
+                    '& .MuiButtonBase-root': {
+                        minWidth: 'auto',
+                    }
+                }}
+            >
+                <Tab label="전체" />
+                <Tab label="상품" />
+                <Tab label="아티클" />
+                <Tab label="비디오" />
+                <Tab label="장소" />
+            </Tabs>
         </Box>
     );
 }
