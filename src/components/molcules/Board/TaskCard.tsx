9b8d4fcda7583f5ce8d6c6d353fed3a0674ae7ee
ShinYoung-Kim <lukey0515@naver.com>
2023-09-26 14:38:08 +0900

    [DAD-856] feat: task card 불필요한 코드 제거

--- src/components/molcules/Board/TaskCard.tsx
+++ src/components/molcules/Board/TaskCard.tsx
@@ -1,21 +1,18 @@
 import TextArea from "@/components/atoms/TextArea";
-import { Task, id } from "@/components/templates/BoardTemplate";
 import { useSortable } from "@dnd-kit/sortable";
 import { Box, Button } from "@mui/material";
 import { useState } from "react";
 import { CSS } from "@dnd-kit/utilities";
 import theme from "@/assets/styles/theme";
+import ScrapCard from "@/components/molcules/Board/ScrapCard";
+import { ScrapOrMemo, Scrap } from "@/types/IBoardAtom";
 
 interface Props {
-    task: Task;
-    deleteTask: (id: id) => void;
-    updateTask: (id: id, content: string) => void;
+    task: ScrapOrMemo;
+    deleteTask: (id: string) => void;
 }
 
-function TaskCard({ task, deleteTask, updateTask }: Props) {
-    const [mouseIsOver, setMouseIsOver] = useState(false);
-    const [editMode, setEditMode] = useState(false);
-
+function TaskCard({ task, deleteTask }: Props) {
     const {
         setNodeRef,
         attributes,
@@ -24,12 +21,11 @@ function TaskCard({ task, deleteTask, updateTask }: Props) {
         transition,
         isDragging,
     } = useSortable({
-        id: task.id,
+        id: task.taskId,
         data: {
             type: 'Task',
             task,
         },
-        disabled: editMode,
     });
 
     const style = {
@@ -37,58 +33,19 @@ function TaskCard({ task, deleteTask, updateTask }: Props) {
         transform: CSS.Transform.toString(transform),
     }
 
-    const toggleEditMode = () => {
-        setEditMode(prev => !prev);
-        setMouseIsOver(false);
-    }
-
     if (isDragging) {
         return <Box
-            ref={setNodeRef}
-            style={style}
             sx={{
                 cursor: 'grab',
                 width: '100%',
-                height: '50px',
-                backgroundColor: theme.color.Gray_080,
                 mb: '10px',
                 opacity: 0.5,
             }}
-        />
-    }
-
-    if (editMode) {
-        return (
-            <Box
-                ref={setNodeRef}
-                style={style}
-                {...attributes}
-                {...listeners}
-                sx={{
-                    cursor: 'grab',
-                    width: '100%',
-                    height: '50px',
-                    backgroundColor: theme.color.Gray_080,
-                    mb: '10px',
-                }}
-                onClick={toggleEditMode}
-            >
-                <textarea
-                    value={task.content}
-                    autoFocus
-                    placeholder="task content"
-                    onBlur={toggleEditMode}
-                    onKeyDown={(e) => {
-                        if (e.key === 'Enter') {
-                            toggleEditMode();
-                        }
-                    }}
-                    onChange={(e) => {
-                        updateTask(task.id, e.target.value);
-                    }}
-                />
-            </Box>
-        )
+            ref={setNodeRef}
+            style={style}
+        >
+            <ScrapCard content={task} />
+        </Box>
     }
 
     return (
@@ -96,20 +53,16 @@ function TaskCard({ task, deleteTask, updateTask }: Props) {
             sx={{
                 cursor: 'grab',
                 width: '100%',
-                height: '50px',
-                backgroundColor: theme.color.Gray_080,
                 mb: '10px',
             }}
-            onMouseEnter={() => setMouseIsOver(true)}
-            onMouseLeave={() => setMouseIsOver(false)}
-            onClick={toggleEditMode}
             ref={setNodeRef}
             style={style}
             {...attributes}
             {...listeners}
         >
-            {task.content}
-            {mouseIsOver
+            <ScrapCard content={task} />
+
+            {/* {mouseIsOver
                 && <Button
                     onClick={() => {
                         deleteTask(task.id)
@@ -122,7 +75,7 @@ function TaskCard({ task, deleteTask, updateTask }: Props) {
                     }}
                 >
                     - Delete
-                </Button>}
+                </Button>} */}
         </Box>
     );
 }
