3f9f7ad709e3d72122644ebd29cbee2a3c4f45a3
ShinYoung-Kim <lukey0515@naver.com>
2023-09-26 14:38:37 +0900

    [DAD-856] feat: board 관련 기능 atom으로 이전

--- src/components/templates/BoardTemplate.tsx
+++ src/components/templates/BoardTemplate.tsx
@@ -1,30 +1,20 @@
 import ColumnContainer from "@/components/molcules/Board/ColumnContainer";
 import { Box, Button } from "@mui/material";
-import { useMemo, useState } from "react";
-import { DndContext, DragEndEvent, DragOverEvent, DragOverlay, DragStartEvent, PointerSensor, useSensor, useSensors } from "@dnd-kit/core";
+import { useEffect, useMemo, useState } from "react";
+import { DndContext, DragEndEvent, DragOverEvent, DragOverlay, DragStartEvent, PointerSensor, closestCenter, useSensor, useSensors } from "@dnd-kit/core";
 import { SortableContext, arrayMove } from "@dnd-kit/sortable";
 import { createPortal } from "react-dom";
 import { create } from "@mui/material/styles/createTransitions";
 import TaskCard from "@/components/molcules/Board/TaskCard";
 import { act } from "react-dom/test-utils";
-
-export type id = string | number;
-export type Column = {
-    id: id;
-    title: string;
-}
-export type Task = {
-    id: id;
-    columnId: id;
-    content: string;
-}
+import { useBoardAtom } from "@/hooks/useBoardAtom";
+import { Column, ScrapOrMemo } from "@/types/IBoardAtom";
 
 function BoardTemplate() {
-    const [columns, setColumns] = useState<Column[]>([]);
-    const columnsId = useMemo(() => columns.map((column) => column.id), [columns]);
-    const [activeColumn, setActiveColumn] = useState<Column | null>(null);
-    const [tasks, setTasks] = useState<Task[]>([]);
-    const [activeTask, setActiveTask] = useState<Task | null>(null);
+    const { board, createNewColumn, onDragEnd, onDragOver, onDragStart, activeTask, activeColumn } = useBoardAtom();
+    const [columns, setColumns] = useState<Column[]>(board.columnList);
+    const columnsId = useMemo(() => columns.map((column) => column.columnId), [columns]);
+    const [tasks, setTasks] = useState<ScrapOrMemo[]>(board.ScrapOrMemoList);
 
     const sensors = useSensors(
         useSensor(PointerSensor, {
@@ -41,6 +31,7 @@ function BoardTemplate() {
                 onDragEnd={onDragEnd}
                 onDragOver={onDragOver}
                 sensors={sensors}
+                collisionDetection={closestCenter}
             >
                 <Box
                     sx={{
@@ -51,16 +42,13 @@ function BoardTemplate() {
                     <SortableContext
                         items={columnsId}
                     >
-                        {columns.map((column) =>
+                        {board.columnList.map((column) =>
                             <ColumnContainer
                                 column={column}
                                 deleteColumn={deleteColumn}
-                                updateColumn={updateColumn}
                                 deleteTask={deleteTask}
-                                key={column.id}
-                                createTask={createTask}
-                                updateTask={updateTask}
-                                tasks={tasks.filter((task) => task.columnId === column.id)}
+                                key={column.columnId}
+                                tasks={board.ScrapOrMemoList.filter((task) => task.columnId === column.columnId)}
                             />
                         )}
                     </SortableContext>
@@ -72,24 +60,20 @@ function BoardTemplate() {
                 </Button>
                 {createPortal(
                     <DragOverlay>
-                        {activeColumn
+                        {/* {activeColumn
                             && <ColumnContainer
                                 column={activeColumn}
                                 deleteColumn={deleteColumn}
-                                updateColumn={updateColumn}
-                                createTask={createTask}
                                 deleteTask={deleteTask}
-                                tasks={tasks.filter(
-                                    (task) => task.columnId === activeColumn.id
+                                tasks={board.ScrapOrMemoList.filter(
+                                    (task) => task.columnId === activeColumn.columnId
                                 )}
-                                updateTask={updateTask}
                             />
-                        }
+                        } */}
                         {activeTask
                             && <TaskCard
                                 task={activeTask}
                                 deleteTask={deleteTask}
-                                updateTask={updateTask}
                             />
                         }
                     </DragOverlay>,
@@ -100,154 +84,18 @@ function BoardTemplate() {
         </Box>
     );
 
-    function createNewColumn() {
-        const columnToAdd: Column = {
-            id: generateId(),
-            title: `Column ${columns.length + 1}`,
-        };
-
-        setColumns([...columns, columnToAdd]);
-    }
-
-    function deleteColumn(id: id) {
-        const newColumns = columns.filter((column) => column.id !== id);
+    function deleteColumn(id: string) {
+        const newColumns = columns.filter((column) => column.columnId !== id);
         setColumns(newColumns);
 
         const newTasks = tasks.filter((task) => task.columnId !== id);
         setTasks(newTasks);
     }
 
-    function onDragStart(event: DragStartEvent) {
-        if (event.active.data.current?.type === 'column') {
-            const column = event.active.data.current?.column;
-            setActiveColumn(column);
-            return;
-        }
-
-        if (event.active.data.current?.type === 'task') {
-            const task = event.active.data.current?.task;
-            setActiveTask(task);
-            return;
-        }
-    }
-
-    function onDragEnd(event: DragEndEvent) {
-        setActiveColumn(null);
-        setActiveTask(null);
-
-        const { active, over } = event;
-        if (!over) {
-            return;
-        }
-
-        const activeColumnId = active.id;
-        const overColumnId = over.id;
-
-        if (activeColumnId === overColumnId) {
-            return;
-        }
-
-        setColumns((columns) => {
-            const overColumnIndex = columns.findIndex((column) => column.id === overColumnId);
-            const activeColumnIndex = columns.findIndex((column) => column.id === activeColumnId);
-
-            return arrayMove(columns, activeColumnIndex, overColumnIndex);
-        });
-    }
-
-    function onDragOver(event: DragOverEvent) {
-        const { active, over } = event;
-        if (!over) {
-            return;
-        }
-
-        const activeColumnId = active.id;
-        const overColumnId = over.id;
-
-        if (activeColumnId === overColumnId) {
-            return;
-        }
-
-        const isActiveATask = active.data.current?.type === 'Task';
-        const isOverATask = over.data.current?.type === 'Task';
-
-        if (!isActiveATask) {
-            return;
-        }
-
-        if (isActiveATask && isOverATask) {
-            setTasks((tasks) => {
-                const activeIndex = tasks.findIndex((task) => task.id === activeColumnId);
-                const overIndex = tasks.findIndex((task) => task.id === overColumnId);
-
-                tasks[activeIndex].columnId = tasks[overIndex].columnId;
-
-                return arrayMove(tasks, activeIndex, overIndex);
-            });
-        }
-
-        const isOverAColumn = over.data.current?.type === 'column';
-
-        if (!isActiveATask && isOverAColumn) {
-            setTasks((tasks) => {
-                const activeIndex = tasks.findIndex((task) => task.id === activeColumnId);
-
-                tasks[activeIndex].columnId = overColumnId;
-
-                return arrayMove(tasks, activeIndex, activeIndex);
-            });
-        }
-    }
-
-    function updateColumn(id: id, title: string) {
-        const newColumns = columns.map((column) => {
-            if (column.id !== id) {
-                return column;
-            }
-
-            return {
-                ...column,
-                title,
-            };
-        });
-
-        setColumns(newColumns);
-    }
-
-    function createTask(columnId: id) {
-        const newTask: Task = {
-            id: generateId(),
-            columnId,
-            content: `Task ${tasks.length + 1}`,
-        };
-
-        setTasks([...tasks, newTask]);
-    }
-
-    function deleteTask(id: id) {
-        const newTask = tasks.filter(task => task.id !== id);
+    function deleteTask(id: string) {
+        const newTask = tasks.filter(task => task.taskId !== id);
         setTasks(newTask);
     }
-
-    function updateTask(id: id, content: string) {
-        const newTasks = tasks.map((task) => {
-            if (task.id !== id) {
-                return task;
-            }
-
-            return {
-                ...task,
-                content,
-            };
-        });
-
-        setTasks(newTasks);
-    }
-
-}
-
-function generateId() {
-    return Math.floor(Math.random() * 10001);
 }
 
 export default BoardTemplate;
