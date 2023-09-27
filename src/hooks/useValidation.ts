6b7926df2acff165c7dab9e6a6429594bda2b7a3
ShinYoung-Kim <lukey0515@naver.com>
2023-09-27 17:55:14 +0900

    [DAD-891] feat: 스크랩 수정하기 모달에 validation 코드 추가 및 메시지 띄우는 로직 연결

--- src/hooks/useValidation.ts
+++ src/hooks/useValidation.ts
@@ -1,5 +1,12 @@
 export const MAX_MEMO_LENGTH = 1000;
 export const SCRAP_LINK_MAX_LENGTH = 2083;
+export const MAX_SCRAP_TITLE_LENGTH = 10;
+export const MAX_SCRAP_DESCRIPTION_LENGTH = 10;
+export const MAX_SCRAP_SITENAME_LENGTH = 10;
+export const MAX_SCRAP_AUTHOR_LENGTH = 10;
+export const MAX_SCRAP_BLOGNAME_LENGTH = 10;
+export const MAX_SCRAP_PRICE_LENGTH = 10;
+export const MAX_SCRAP_CHANNELNAME_LENGTH = 10;
 
 export function useIsValidURL(url: string) {
     try {
