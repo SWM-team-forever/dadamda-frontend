83c6fc8f5c6c5bf90aeb8baf76f21f7775bc465f
ShinYoung-Kim <lukey0515@naver.com>
2023-09-27 15:37:26 +0900

    [DAD-891] feat: 공백으로 이루어진 text 검사

--- src/hooks/useValidation.ts
+++ src/hooks/useValidation.ts
@@ -10,3 +10,7 @@ export function useIsValidURL(url: string) {
 export function useIsLessThanLengthLimitation(text: string, limit: number) {
     return text.length <= limit;
 }
+
+export function useIsBlank(text: string) {
+    return text.replace(/\s+/g, '').length === 0;
+}
