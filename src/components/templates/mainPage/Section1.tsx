b4f48dbca8e78ce2d5509ab631ddb7ce447e5cf7
ShinYoung-Kim <lukey0515@naver.com>
2023-09-28 15:10:14 +0900

    [DAD-898] feat: section1 이미지 적용

--- src/components/templates/mainPage/Section1.tsx
+++ src/components/templates/mainPage/Section1.tsx
@@ -1,14 +1,20 @@
 import theme from "@/assets/styles/theme";
 import { Box, Grid, Typography, Button } from "@mui/material";
 
+import section1Image from "@/assets/images/landing/section1/section1Image.png";
+import section1Text1 from "@/assets/images/landing/section1/section1Text1.png";
+import section1Text2 from "@/assets/images/landing/section1/section1Text2.png";
+
 function Section1() {
     return (
         <Box sx={{
             width: '100%',
             display: 'flex',
-            minHeight: '600px',
+            height: '100%',
             alignItems: 'center',
             justifyContent: 'center',
+            backgroundColor: 'white',
+            overflow: 'hidden',
         }}>
             <Grid container spacing={6} sx={{
                 display: 'flex',
@@ -17,21 +23,22 @@ function Section1() {
                 padding: '50px',
             }}>
                 <Grid item xs={12} md={5}>
-                    <Box
-                        sx={{
-                            backgroundColor: theme.color.Blue_090
-                        }}
-                    >
-                        left
+                    <Box>
+                        <img src={section1Text1} alt="section1Text1" style={{
+                            width: '100%',
+                        }} />
+                        <img src={section1Text2} alt="section1Text2" style={{
+                            width: '100%',
+                        }} />
                     </Box>
                 </Grid>
                 <Grid item xs={12} md={7}>
                     <Box
-                        sx={{
-                            backgroundColor: theme.color.Gray_090
-                        }}
+
                     >
-                        right
+                        <img src={section1Image} alt="section1Image" style={{
+                            width: '100%',
+                        }} />
                     </Box>
                 </Grid>
             </Grid>
