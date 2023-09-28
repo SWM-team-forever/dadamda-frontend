8f753f02669a09622b7f04b0467a83554c3ac014
ShinYoung-Kim <lukey0515@naver.com>
2023-09-28 15:03:39 +0900

    [DAD-898] feat: section1 레이아웃 추가

--- src/components/templates/mainPage/Section1.tsx
+++ src/components/templates/mainPage/Section1.tsx
@@ -1,8 +1,41 @@
+import theme from "@/assets/styles/theme";
+import { Box, Grid, Typography, Button } from "@mui/material";
+
 function Section1() {
     return (
-        <div>
-
-        </div>
+        <Box sx={{
+            width: '100%',
+            display: 'flex',
+            minHeight: '600px',
+            alignItems: 'center',
+            justifyContent: 'center',
+        }}>
+            <Grid container spacing={6} sx={{
+                display: 'flex',
+                alignItems: 'center',
+                maxWidth: '1300px',
+                padding: '50px',
+            }}>
+                <Grid item xs={12} md={5}>
+                    <Box
+                        sx={{
+                            backgroundColor: theme.color.Blue_090
+                        }}
+                    >
+                        left
+                    </Box>
+                </Grid>
+                <Grid item xs={12} md={7}>
+                    <Box
+                        sx={{
+                            backgroundColor: theme.color.Gray_090
+                        }}
+                    >
+                        right
+                    </Box>
+                </Grid>
+            </Grid>
+        </Box>
     )
 }
 
