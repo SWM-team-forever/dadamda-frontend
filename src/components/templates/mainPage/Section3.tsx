25be4a8cb2c2da4aa412361c2e198dc77e1f4807
ShinYoung-Kim <lukey0515@naver.com>
2023-09-28 16:34:50 +0900

    [DAD-898] feat: section3 이미지 적용

--- src/components/templates/mainPage/Section3.tsx
+++ src/components/templates/mainPage/Section3.tsx
@@ -1,7 +1,11 @@
+import section3Image1 from '@/assets/images/landing/section3/section3Image1.png';
+import section3Image2 from '@/assets/images/landing/section3/section3Image2.png';
+
 function Section3() {
     return (
         <div>
-
+            <img src={section3Image1} alt="section3Image1" style={{}} />
+            <img src={section3Image2} alt="section3Image2" style={{}} />
         </div>
     );
 }
