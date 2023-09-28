a0c67dec05298bc334a92645735258489cd661fa
ShinYoung-Kim <lukey0515@naver.com>
2023-09-28 16:16:01 +0900

    [DAD-898] style: section2 스타일 적용

--- src/components/templates/mainPage/Section2.tsx
+++ src/components/templates/mainPage/Section2.tsx
@@ -1,6 +1,14 @@
+import theme from "@/assets/styles/theme";
 import { Box, Grid, Typography } from "@mui/material";
 
 function Section2() {
+    const fontStyle = {
+        fontSize: '32px',
+        fontWeight: '700',
+        lineHeight: '160%',
+        color: theme.color.Gray_090,
+    };
+
     return (
         <Box sx={{
             width: '100%',
@@ -16,22 +24,48 @@ function Section2() {
             },
             boxSizing: 'border-box',
         }}>
-            <Grid container spacing={{
-                xs: '0',
-                md: '6',
-            }} sx={{
-                display: 'flex',
-                alignItems: 'flex-start',
-                maxWidth: '1300px',
-                boxSizing: 'border-box',
-                m: '0',
-                justifyContent: 'space-around',
-            }}>
-                <Typography variant="h4" component="h2" sx={{}}>
-                    이제, 다담다로 북마크의 신세계를 경험하세요
+            <Grid container
+                spacing={{
+                    xs: '0',
+                    md: '6',
+                }}
+                sx={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    maxWidth: '1300px',
+                    boxSizing: 'border-box',
+                    m: '0',
+                    justifyContent: 'center',
+                    gap: '10px',
+                }}>
+                <Typography sx={fontStyle}>
+                    이제,
                 </Typography>
-            </Grid>
-        </Box>
+                <Box
+                    sx={{
+                        backgroundColor: theme.color.Blue_080,
+                        borderRadius: '100%',
+                        aspectRatio: '1/1',
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        transform: 'scale(1.2)',
+                    }}
+                >
+                    <Typography
+                        sx={{
+                            ...fontStyle,
+                            color: 'white',
+                            transform: 'scale(0.8)',
+                        }}>
+                        다담다
+                    </Typography>
+                </Box>
+                <Typography sx={fontStyle}>
+                    로 북마크의 신세계를 경험하세요
+                </Typography>
+            </Grid >
+        </Box >
     );
 }
 
