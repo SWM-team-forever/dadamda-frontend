4afda310c9e0c1108ecb7789d07ef1d34ede460f
ShinYoung-Kim <lukey0515@naver.com>
2023-09-29 01:31:20 +0900

    [DAD-898] feat: footer 레이아웃 완료

--- src/components/templates/mainPage/Footer.tsx
+++ src/components/templates/mainPage/Footer.tsx
@@ -2,6 +2,24 @@ import { Box, Typography, Link, Grid } from '@mui/material';
 import theme from '@/assets/styles/theme';
 
 const Footer = () => {
+    const boldTextStyle = {
+        color: theme.color.Gray_090,
+        fontSize: '14px',
+        fontWeight: '700',
+        lineHeight: '140%',
+        textAlign: 'center',
+        wordBreak: 'keep-all',
+    };
+
+    const normalTextStyle = {
+        color: theme.color.Gray_080,
+        fontSize: '12px',
+        fontWeight: '300',
+        lineHeight: '166%',
+        textAlign: 'center',
+        wordBreak: 'keep-all',
+    }
+
     return (
         <Box sx={{
             flexGrow: 1,
@@ -17,14 +35,66 @@ const Footer = () => {
                 alignItems: 'center',
                 justifyContent: 'center',
                 gap: '10px',
+                mt: '30px',
             }}>
-                <Grid item xs={12} sm={5}>
-                    <Typography>(주) 다담다</Typography>
-                    <Typography sx={{ fontSize: '0.625rem', }}>서울특별시 강남구 테헤란로 311(역삼동) 아남타워빌딩 7층(06151)</Typography>
-                    <Typography sx={{ fontSize: '0.625rem', }}>기업 또는 이용 문의: dadamda.corporation@gmail.com</Typography>
+                <Grid item xs={12} sm={7}>
+                    <Box
+                        sx={{
+                            display: 'flex',
+                            flexDirection: {
+                                xs: 'column',
+                                sm: 'row',
+                            },
+                        }}
+                    >
+                        <Typography
+                            sx={{
+                                ...boldTextStyle,
+                                mr: {
+                                    xs: '0',
+                                    sm: '50px',
+                                },
+                                mb: {
+                                    xs: '10px',
+                                    sm: '0',
+                                },
+                                whiteSpace: 'nowrap',
+                            }}
+                        >
+                            (주) 다담다
+                        </Typography>
+                        <Box>
+                            <Typography
+                                sx={{
+                                    ...normalTextStyle,
+                                    textAlign: {
+                                        xs: 'center',
+                                        sm: 'left',
+                                    }
+                                }}
+                            >
+                                서울특별시 강남구 테헤란로 311(역삼동) 아남타워빌딩 7층(06151)
+                            </Typography>
+                            <Typography
+                                sx={{
+                                    ...normalTextStyle,
+                                    textAlign: {
+                                        xs: 'center',
+                                        sm: 'left',
+                                    }
+                                }}
+                            >
+                                기업 또는 이용 문의: dadamda.corporation@gmail.com
+                            </Typography>
+                        </Box>
+                    </Box>
                 </Grid>
-                <Grid item xs={12} sm={5}>
-                    <Typography sx={{ fontSize: '0.625rem', }}>
+                <Grid item xs={12} sm={4}>
+                    <Typography
+                        sx={{
+                            ...boldTextStyle
+                        }}
+                    >
                         <Link href='/privacy' target="_blank" underline="none" style={{
                             textDecoration: 'none',
                             color: theme.color.text_gray_color,
@@ -34,10 +104,21 @@ const Footer = () => {
                     </Typography>
                 </Grid>
                 <Grid item xs={12} sm={12}>
-                    <Typography sx={{ fontSize: '0.625rem', }}>COPYRIGHT 2023 dadamda Co. All RIGHTS RESERVED.</Typography>
+                    <Typography
+                        sx={{
+                            ...normalTextStyle,
+                            mb: '30px',
+                            mt: {
+                                xs: '24px',
+                                sm: '30px',
+                            }
+                        }}
+                    >
+                        COPYRIGHT 2023 dadamda Co. All RIGHTS RESERVED.
+                    </Typography>
                 </Grid>
             </Grid>
-        </Box>
+        </Box >
     );
 };
 
