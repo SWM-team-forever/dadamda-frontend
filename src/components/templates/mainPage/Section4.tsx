402c69e4e404dace928d1a50fd1fa6aa4460de5f
ShinYoung-Kim <lukey0515@naver.com>
2023-09-28 21:17:56 +0900

    [DAD-898] feat: section4에 배경 Box 컴포넌트 추가

--- src/components/templates/mainPage/Section4.tsx
+++ src/components/templates/mainPage/Section4.tsx
@@ -1,8 +1,22 @@
+import sectionBlueGradationBackground from '@/assets/images/landing/section4/sectionBlueGradationBackground.png';
+import { Box } from '@mui/material';
+
 function Section4() {
     return (
-        <div>
-
-        </div>
+        <Box sx={{
+            width: '100%',
+            display: 'flex',
+            height: 'calc(100% + 56px)',
+            alignItems: 'flex-start',
+            justifyContent: 'center',
+            overflow: 'hidden',
+            boxSizing: 'border-box',
+            backgroundImage: `url(${sectionBlueGradationBackground})`,
+            backgroundRepeat: 'no-repeat',
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+        }}>
+        </Box>
     )
 }
 
