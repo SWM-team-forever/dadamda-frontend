f75b83cb970dab19bb923d009cda2b9c94160d89
ShinYoung-Kim <lukey0515@naver.com>
2023-09-28 22:43:45 +0900

    [DAD-898] feat: section5에 이미지 적용

--- src/components/templates/mainPage/Section5.tsx
+++ src/components/templates/mainPage/Section5.tsx
@@ -1,12 +1,15 @@
 import sectionBlueGradationBackground from '@/assets/images/landing/section4/sectionBlueGradationBackground.png';
-import { Box } from '@mui/material';
+import section5Image from '@/assets/images/landing/section4/section5Image.png';
+import leftHereTooltip from '@/assets/images/landing/section4/leftHereTooltip.png';
+import theme from '@/assets/styles/theme';
+import { Box, Grid, Typography } from '@mui/material';
 
 function Section4() {
     return (
         <Box sx={{
             width: '100%',
             display: 'flex',
-            height: 'calc(100% + 56px)',
+            maxHeight: 'calc(100% + 56px)',
             alignItems: 'flex-start',
             justifyContent: 'center',
             overflow: 'hidden',
@@ -16,7 +19,83 @@ function Section4() {
             backgroundSize: 'cover',
             backgroundPosition: 'center',
         }}>
-        </Box>
+            <Grid
+                container
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'flex-start',
+                    maxWidth: '1300px',
+                    boxSizing: 'border-box',
+                    m: '0',
+                    justifyContent: 'space-around',
+                }}
+            >
+                <Grid item xs={10} sm={10}
+                    sx={{
+                        '&.MuiGrid-item': {
+                            m: {
+                                xs: '80px 0 60px 40px',
+                                sm: '120px 0 0 120px',
+                            },
+                            boxSizing: 'border-box',
+                        },
+                    }}
+                >
+                    <Box
+                        sx={{
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'flex-start',
+                            justifyContent: 'center',
+                            gap: '15px',
+                            p: '0',
+                        }}
+                    >
+                        <Typography sx={{
+                            color: theme.color.Blue_080,
+                            fontSize: '18px',
+                            fontWeight: '700',
+                            lineHeight: '160%',
+                        }}>
+                            # 자동 컨텐츠 맞춤 정보 추출
+                        </Typography>
+                        <Typography sx={{
+                            fontSize: '28px',
+                            fontWeight: '700',
+                            lineHeight: '150%',
+                            wordBreak: 'keep-all',
+                        }}>
+                            컨텐츠의 정보를<br />
+                            번거롭게 작성할 필요 없어요.<br />
+                            그저 저장만 하세요.
+                        </Typography>
+                    </Box>
+                </Grid>
+                <Grid item xs={10} sm={7}
+                    sx={{
+                        '&.MuiGrid-item': {
+                            p: '0',
+                        },
+                        alignSelf: 'flex-end'
+                    }}
+                >
+                    <Box
+                        sx={{
+                            display: 'flex',
+                            alignItems: 'center',
+                        }}
+                    >
+                        <img src={leftHereTooltip} alt="rightHereTooltip" style={{
+                            width: '75px',
+                        }} />
+                        <img src={section5Image} alt="section4Image" style={{
+                            width: '100%',
+                        }} />
+                    </Box>
+                </Grid>
+            </Grid>
+        </Box >
     );
 }
 
