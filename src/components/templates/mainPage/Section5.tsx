b606c2a512199169ce5acd629e73a6b597a10069
ShinYoung-Kim <lukey0515@naver.com>
2023-09-28 21:20:25 +0900

    [DAD-898] feat: section5에 배경 이미지 적용

--- src/components/templates/mainPage/Section5.tsx
+++ src/components/templates/mainPage/Section5.tsx
@@ -1,9 +1,23 @@
-function Section5() {
-    return (
-        <div>
+import sectionBlueGradationBackground from '@/assets/images/landing/section4/sectionBlueGradationBackground.png';
+import { Box } from '@mui/material';
 
-        </div>
-    )
+function Section4() {
+    return (
+        <Box sx={{
+            width: '100%',
+            display: 'flex',
+            height: 'calc(100% + 56px)',
+            alignItems: 'flex-start',
+            justifyContent: 'center',
+            overflow: 'hidden',
+            boxSizing: 'border-box',
+            backgroundImage: `url(${sectionBlueGradationBackground})`,
+            backgroundRepeat: 'no-repeat',
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+        }}>
+        </Box>
+    );
 }
 
-export default Section5
+export default Section4;
