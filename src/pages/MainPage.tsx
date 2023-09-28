c85f21731fe3523baffc51e7018c7fb7c9e7ba69
ShinYoung-Kim <lukey0515@naver.com>
2023-09-28 16:14:46 +0900

    [DAD-898] style: 메인 페이지 사이즈 변경

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,12 +1,10 @@
 import styled from 'styled-components';
-import theme from '../assets/styles/theme';
-import Button from '../components/atoms/DefaultButton';
+import { useEffect } from 'react';
+
 import ChannelService from '../../ChannelService';
 import { CHANNEL_SERVICE_PLUGIN_KEY } from '../secret';
-import { useEffect } from 'react';
-import { Typography } from '@mui/material';
+
 import Hero from '../components/templates/mainPage/Hero';
-import Howto from '../components/templates/mainPage/Howto';
 import Footer from '../components/templates/mainPage/Footer';
 import Section1 from '@/components/templates/mainPage/Section1';
 import Section2 from '@/components/templates/mainPage/Section2';
@@ -33,9 +31,9 @@ function MainPage() {
 
 const BackgroundContainer = styled.div`    
     width: 100%;
-    height: calc(100vh - 50px);
+    height: calc(100vh - 56px);
     position: fixed;
-    top: 50px;
+    top: 56px;
     overflow: auto;
 `
 
