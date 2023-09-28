6a0e62e47e6b80791d2dec4abb6dfa34775017c7
ShinYoung-Kim <lukey0515@naver.com>
2023-09-29 00:51:25 +0900

    [DAD-898] style: section 6 레이아웃 적용

--- src/components/templates/mainPage/Section6.tsx
+++ src/components/templates/mainPage/Section6.tsx
@@ -1,8 +1,128 @@
+import { Box, Grid, Typography } from "@mui/material";
+import section6BackgroundImage from '@/assets/images/landing/section6/section6BackgroundImage.png';
+
 function Section6() {
     return (
-        <div>
+        <Box sx={{
+            width: '100%',
+            display: 'flex',
+            maxHeight: 'calc(100% + 56px)',
+            alignItems: 'flex-start',
+            justifyContent: 'center',
+            overflow: 'hidden',
+            boxSizing: 'border-box',
+            backgroundImage: `url(${section6BackgroundImage})`,
+            backgroundRepeat: 'no-repeat',
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+        }}>
+            <Grid container spacing={6}
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    maxWidth: '1300px',
+                    m: 0,
+                    justifyContent: 'center',
+                    height: '100%',
+                }}>
+                <Grid item xs={12} sm={4}
+                    sx={{
+                        '&.MuiGrid-item': {
+                            p: '0',
+                            mt: '50px',
+                        },
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        gap: '20px',
+                        p: '0',
+                        boxSizing: 'border-box',
+                    }}
+                >
+                    <Box
+                        sx={{
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                            gap: {
+                                xs: '8px',
+                                sm: '16px',
+                            }
+                        }}
+                    >
+                        <Typography sx={{
+                            color: '#FFBB36',
+                            fontSize: '18px',
+                            fontWeight: '700',
+                            lineHeight: '160%',
 
-        </div>
+                        }}>
+                            # 빠른 저장
+                        </Typography>
+                        <Typography sx={{
+                            color: 'white',
+                            fontSize: {
+                                xs: '26px',
+                                sm: '32px',
+                            },
+                            fontWeight: '700',
+                            lineHeight: '140%',
+                            textAlign: 'center',
+                            whiteSpace: 'nowrap',
+                        }}>
+                            클릭 한번으로 링크 저장!<br />
+                            그것도 보기 좋게.
+                        </Typography>
+                        <Typography sx={{
+                            color: 'white',
+                            fontSize: '16px',
+                            fontWeight: '400',
+                            lineHeight: '150%',
+                            textAlign: 'center',
+                            whiteSpace: 'nowrap',
+                        }}>
+                            저장한 링크는 콘텐츠로 저장해줘요.
+                        </Typography>
+                    </Box>
+                    {/* <img src={section3Image1} alt="section3Image1" style={{
+                        width: '100%',
+                    }} /> */}
+                </Grid>
+                <Grid item xs={12} sm={8}
+                    sx={{
+                        '&.MuiGrid-item': {
+                            p: '0',
+                        },
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        gap: '20px',
+                        p: '0',
+                        boxSizing: 'border-box',
+                        transform: {
+                            xs: 'translateY(50%)',
+                            sm: 'translateY(0)',
+                        }
+                    }}
+                >
+                    <Box
+                        sx={{
+                            transform: {
+                                xs: 'scale(2)',
+                                sm: 'scale(1)',
+                            },
+                        }}
+                    >
+                        {/* <img src={section3Image2} alt="section3Image2" style={{
+                            width: '100%',
+                        }} /> */}
+                    </Box>
+                </Grid>
+            </Grid>
+        </Box>
     );
 }
 
