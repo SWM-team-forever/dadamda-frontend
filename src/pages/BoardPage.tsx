b64be10611e944bc28be92926b52b81ad1d735cb
ShinYoung-Kim <lukey0515@naver.com>
2023-10-04 02:18:55 +0900

    [DAD-856] feat: 불필요한 wrapper 컴포넌트 제거

--- src/pages/BoardPage.tsx
+++ src/pages/BoardPage.tsx
@@ -2,15 +2,12 @@ import styled from 'styled-components';
 import { Outlet } from 'react-router-dom';
 
 import Navbar from '../components/molcules/Navigation/Navbar';
-import theme from '../assets/styles/theme';
 
 function BoardPage() {
     return (
         <PageWrapper>
             <Navbar />
-            <OutletWrapper>
-                <Outlet />
-            </OutletWrapper>
+            <Outlet />
         </PageWrapper >
     );
 }
@@ -19,21 +16,6 @@ const PageWrapper = styled.div`
    display: flex;
     width: 100%;
     height: 100%; 
-`
-
-const OutletWrapper = styled.div`
-    width: calc(100vw - 200px);
-    height: calc(100vh - 50px);
-    background-color: ${theme.color.background_color};
-    position: fixed;
-    right: 0;
-    top: 50px;
-    display: flex;
-    overflow: auto;
-    @media screen and (max-width: 600px) {
-      width: 100vw;
-      left: 0;
-    }
-`
+`;
 
 export default BoardPage;
