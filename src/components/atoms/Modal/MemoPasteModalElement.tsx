f29db605be3e47adb46614146c35a214373a37a1
ShinYoung-Kim <lukey0515@naver.com>
2023-10-10 11:15:38 +0900

    [DAD-922] feat: 보드 메모 추가 모달 모양 복제

--- src/components/atoms/Modal/MemoPasteModalElement.tsx
+++ src/components/atoms/Modal/MemoPasteModalElement.tsx
@@ -1,8 +1,102 @@
-function MemoPasteModalElement() {
-    return (
-        <div>
+import { Box, Button, FormControl, FormHelperText, OutlinedInput, Typography } from '@mui/material';
+import { useState, useEffect, ChangeEvent } from 'react';
 
-        </div>
+import theme from '@/assets/styles/theme';
+import { useModal } from '@/hooks/useModal';
+import { MAX_MEMO_LENGTH, useIsBlank, useIsEntered, useIsLessThanLengthLimitation } from '@/hooks/useValidation';
+
+function MemoPasteModalElement() {
+    const [, setToken] = useState<string | null>(null);
+    useEffect(() => {
+        setToken(localStorage.getItem('token'));
+    }, []);
+
+    const { closeModal } = useModal();
+    const [textAreaValue, setTextAreaValue] = useState('');
+
+    const handleSetValue = (e: ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
+        e.preventDefault();
+        setTextAreaValue(e.target.value);
+    }
+
+    const validation = () => {
+        if (!useIsLessThanLengthLimitation(textAreaValue, MAX_MEMO_LENGTH)) {
+            return `최대 ${MAX_MEMO_LENGTH}글자까지만 입력 가능합니다.`;
+        }
+
+        if (!useIsEntered(textAreaValue)) {
+            return ' ';
+        }
+
+        if (useIsBlank(textAreaValue)) {
+            return '공백만 입력되었습니다.';
+        }
+
+        return 'success';
+    }
+    const isValidationSuccess = () => validation() === 'success';
+
+    return (
+        <Box
+            sx={{
+                p: '0 16px',
+                display: 'flex',
+                flexDirection: 'column',
+            }}
+        >
+            <FormControl>
+                <OutlinedInput
+                    placeholder="추가할 메모를 입력하세요."
+                    onChange={(e) => handleSetValue(e)}
+                    sx={{
+                        width: '100%',
+                        fontWeight: '500',
+                        fontHeight: '150%',
+                        p: '14px 21px',
+                        '& fieldset': {
+                            color: theme.color.Gray_060,
+                        },
+                    }}
+                    multiline
+                    rows={5}
+                />
+                <FormHelperText>
+                    <Typography
+                        variant="h6"
+                        color={isValidationSuccess() ? theme.color.Gray_060 : '#f44336'}
+                        sx={{
+                            fontWeight: '500',
+                            lineHeight: '150%',
+                            mt: '6px',
+                        }}
+                    >
+                        {isValidationSuccess() ? `${textAreaValue.length} / ${MAX_MEMO_LENGTH}자` : validation()}
+                    </Typography>
+                </FormHelperText>
+            </FormControl>
+            <Button
+                variant='contained'
+                sx={{
+                    backgroundColor: theme.color.Gray_050,
+                    borderRadius: '4px',
+                    boxShadow: 'none',
+                    width: 'fit-content',
+                    alignSelf: 'flex-end',
+                    '&:hover': {
+                        backgroundColor: theme.color.Blue_080,
+                        boxShadow: 'none',
+                    }
+                }}
+                onClick={
+                    () => {
+                        closeModal();
+                    }
+                }
+                disabled={!isValidationSuccess()}
+            >
+                등록
+            </Button>
+        </Box>
     );
 }
 
