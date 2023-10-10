ce7ca26632c03f2d948e0c407e3e5b54115af73c
ShinYoung-Kim <lukey0515@naver.com>
2023-10-11 00:27:31 +0900

    [DAD-923] feat: 보드 api 작업

--- src/api/board.ts
+++ src/api/board.ts
@@ -1,5 +1,5 @@
 import { useDefaultSnackbar } from "@/hooks/useWarningSnackbar";
-import { GET_BOARD_LIST_URL, POST_CREATE_BOARD_URL } from "@/secret";
+import { DELETE_BOARD_URL, EDIT_BOARD_URL, GET_BOARD_LIST_URL, GET_BOARD_URL, POST_CREATE_BOARD_URL } from "@/secret";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import * as Sentry from '@sentry/react';
 
@@ -78,6 +78,115 @@ export const usePostCreateBoard = () => {
         onError: (error) => {
             Sentry.captureException(error);
             useDefaultSnackbar('보드 생성에 실패하였습니다.', 'error');
+        },
+        useErrorBoundary: false,
+    });
+}
+
+const getBoard = async (boardId: string) => {
+    const response = token && await fetch(`${GET_BOARD_URL}/${boardId}`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "X-AUTH-TOKEN": token,
+        },
+    }).then((response) => {
+        return response.json().then(body => {
+            if (response.ok) {
+                return body;
+            } else {
+                throw new Error(body.resultCode);
+            }
+        })
+    });
+
+    return response;
+}
+
+export const useGetBoard = async (boardId: string) => {
+    const board = await getBoard(boardId);
+    return board;
+}
+
+const deleteBoard = async (boardId: string) => {
+    const response = token && await fetch(`${DELETE_BOARD_URL}/${boardId}`, {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json",
+            "X-AUTH-TOKEN": token,
+        },
+    }).then((response) => {
+        return response.json().then(body => {
+            if (response.ok) {
+                return body;
+            } else {
+                throw new Error(body.resultCode);
+            }
+        })
+    });
+
+    return response;
+}
+
+export const useDeleteBoard = () => {
+    const queryClient = useQueryClient();
+
+    return useMutation(deleteBoard, {
+        onSuccess: () => {
+            queryClient.invalidateQueries(['boards']);
+            useDefaultSnackbar('보드가 삭제되었습니다', 'success');
+        },
+        onError: (error) => {
+            Sentry.captureException(error);
+            useDefaultSnackbar('보드 삭제에 실패하였습니다.', 'error');
+        },
+        useErrorBoundary: false,
+    });
+}
+
+interface editBoardProps {
+    boardId: string,
+    name: string,
+    description: string,
+    tag: string,
+}
+
+const editBoard = async ({boardId, description, tag, name}: editBoardProps) => {
+    const response = token && await fetch(`${EDIT_BOARD_URL}/${boardId}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+            "X-AUTH-TOKEN": token,
+        },
+        body: JSON.stringify({
+            name: name,
+            description: description,
+            tag: tag,
+        }),
+    }).then((response) => {
+        return response.json().then(body => {
+            if (response.ok) {
+                return body;
+            } else {
+                throw new Error(body.resultCode);
+            }
+        })
+    });
+
+    return response;
+}
+
+export const useEditBoard = () => {
+    const queryClient = useQueryClient();
+
+    return useMutation(editBoard, {
+        onSuccess: () => {
+            queryClient.invalidateQueries(['boards']);
+            useDefaultSnackbar('보드가 수정되었습니다', 'success');
+        },
+        onError: (error) => {
+            Sentry.captureException(error);
+            useDefaultSnackbar('보드 수정에 실패하였습니다.', 'error');
         },
         useErrorBoundary: false,
     });
