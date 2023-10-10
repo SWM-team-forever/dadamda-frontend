72f0a1e094f78ca92e22bec2341985c12be765c7
ShinYoung-Kim <lukey0515@naver.com>
2023-10-10 14:51:52 +0900

    [DAD-856] style: 열 이동 삭제 버튼 styling

--- src/components/templates/BB.jsx
+++ src/components/templates/BB.jsx
@@ -25,8 +25,7 @@ import {
 } from '@dnd-kit/sortable';
 import { CSS } from '@dnd-kit/utilities';
 import ScrapCard from '@/components/molcules/Board/ScrapCard.tsx';
-import { Box, Button } from '@mui/material';
-import Memo from '../molcules/Memo';
+import { Box } from '@mui/material';
 import { useBoardContentAtom } from '@/hooks/useBoardContentAtom';
 import Sticker from '../molcules/Board/Sticker';
 import theme from '@/assets/styles/theme';
@@ -41,14 +40,20 @@ export const Action = forwardRef(
                 ref={ref}
                 {...props}
                 tabIndex={0}
-                style={
-                    {
-                        ...style,
-                        cursor,
-                        '--fill': active?.fill,
-                        '--background': active?.background,
-                    }
-                }
+                style={{
+                    ...style,
+                    cursor,
+                    backgroundColor: theme.color.Gray_020,
+                    border: 'none',
+                    borderRadius: '4px',
+                    padding: '5px',
+                    boxSizing: 'border-box',
+                    width: '20px',
+                    height: '20px',
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                }}
             />
         );
     }
@@ -63,8 +68,8 @@ export const Handle = forwardRef(
                 data-cypress="draggable-handle"
                 {...props}
             >
-                <svg viewBox="0 0 20 20" width="12">
-                    <path d="M7 2a2 2 0 1 0 .001 4.001A2 2 0 0 0 7 2zm0 6a2 2 0 1 0 .001 4.001A2 2 0 0 0 7 8zm0 6a2 2 0 1 0 .001 4.001A2 2 0 0 0 7 14zm6-8a2 2 0 1 0-.001-4.001A2 2 0 0 0 13 6zm0 2a2 2 0 1 0 .001 4.001A2 2 0 0 0 13 8zm0 6a2 2 0 1 0 .001 4.001A2 2 0 0 0 13 14z"></path>
+                <svg viewBox="0 0 20 20" fill="#919eab">
+                <path d="M7 2a2 2 0 1 0 .001 4.001A2 2 0 0 0 7 2zm0 6a2 2 0 1 0 .001 4.001A2 2 0 0 0 7 8zm0 6a2 2 0 1 0 .001 4.001A2 2 0 0 0 7 14zm6-8a2 2 0 1 0-.001-4.001A2 2 0 0 0 13 6zm0 2a2 2 0 1 0 .001 4.001A2 2 0 0 0 13 8zm0 6a2 2 0 1 0 .001 4.001A2 2 0 0 0 13 14z" fill='black'></path>
                 </svg>
             </Action>
         );
@@ -74,13 +79,10 @@ export const Handle = forwardRef(
 export function Remove(props) {
     return (
         <Action
+            cursor="pointer"
             {...props}
-            active={{
-                fill: 'rgba(255, 70, 70, 0.95)',
-                background: 'rgba(255, 70, 70, 0.1)',
-            }}
         >
-            <svg width="8" viewBox="0 0 22 22" xmlns="http://www.w3.org/2000/svg">
+            <svg width="6" viewBox="0 0 22 22" xmlns="http://www.w3.org/2000/svg">
                 <path d="M2.99998 -0.000206962C2.7441 -0.000206962 2.48794 0.0972617 2.29294 0.292762L0.292945 2.29276C-0.0980552 2.68376 -0.0980552 3.31682 0.292945 3.70682L7.58591 10.9998L0.292945 18.2928C-0.0980552 18.6838 -0.0980552 19.3168 0.292945 19.7068L2.29294 21.7068C2.68394 22.0978 3.31701 22.0978 3.70701 21.7068L11 14.4139L18.2929 21.7068C18.6829 22.0978 19.317 22.0978 19.707 21.7068L21.707 19.7068C22.098 19.3158 22.098 18.6828 21.707 18.2928L14.414 10.9998L21.707 3.70682C22.098 3.31682 22.098 2.68276 21.707 2.29276L19.707 0.292762C19.316 -0.0982383 18.6829 -0.0982383 18.2929 0.292762L11 7.58573L3.70701 0.292762C3.51151 0.0972617 3.25585 -0.000206962 2.99998 -0.000206962Z" />
             </svg>
         </Action>
@@ -125,14 +127,17 @@ export const Container = forwardRef(
                 tabIndex={onClick ? 0 : undefined}
             >
                 {label ? (
-                    <div>
-                        {label}
-                        <div>
-                            {onRemove ? <Remove onClick={onRemove} /> : undefined}
-                            <Handle {...handleProps} />
-                        </div>
-                    </div>
-                ) : null}
+                <Box
+                sx={{
+                    display: 'flex',
+                   justifyContent: 'flex-end',
+                    width: '100%',
+                    gap: '5px',
+                }}
+                >
+                    {onRemove ? <Remove onClick={onRemove} /> : undefined}
+                    <Handle {...handleProps} />
+                </Box>) : null}
                 {placeholder ? children : <ul
                 style={{
                     display: 'flex',
