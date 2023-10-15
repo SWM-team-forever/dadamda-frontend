d880bcca9613cc7bb175ef0939c0d90ddca461f5
ShinYoung-Kim <lukey0515@naver.com>
2023-10-16 04:12:45 +0900

    [DAD-972] feat: 로그아웃 버튼에 amplitude 연결

--- src/hooks/useAccount.ts
+++ src/hooks/useAccount.ts
@@ -1,7 +1,9 @@
+import { logEvent } from "@/utility/amplitude";
 import { useNavigate } from "react-router-dom";
 
 export function useLogout() {
     const navigate = useNavigate();
+    logEvent('logout');
     return () => {
         localStorage.removeItem('token');
         localStorage.removeItem('profileImageURL');
