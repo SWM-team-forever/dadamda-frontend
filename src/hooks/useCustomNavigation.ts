104cc3c4e88a1567f40dd4dfc09f16be66f9d0c4
ShinYoung-Kim <lukey0515@naver.com>
2023-10-16 04:09:45 +0900

    [DAD-973] feat: 크롬 익스텐션 설치하기 버튼 클릭 amplitude 연결

--- src/hooks/useCustomNavigation.ts
+++ src/hooks/useCustomNavigation.ts
@@ -1,5 +1,8 @@
+import { logEvent } from "@/utility/amplitude";
+
 const chromeExtensionInstallLink = 'https://chrome.google.com/webstore/detail/dadamda/kgaiabolccidmgihificdfaimdlfmcfj?hl=ko';
 
 export function useMoveToChromeExtensionInstallLink() {
+    logEvent('chrome_extension_install_link_click');
     location.href = chromeExtensionInstallLink;
 }
