2dfab9257834b7a8df94878f5cd82c0d0b26da64
ShinYoung-Kim <lukey0515@naver.com>
2023-10-18 22:17:59 +0900

    [DAD-1007] feat: 공유하기 모달 ui 구성 구현

--- src/components/atoms/Modal/BoardShareModalElement.tsx
+++ src/components/atoms/Modal/BoardShareModalElement.tsx
@@ -1,11 +1,44 @@
-import { Switch, Typography } from "@mui/material";
+import { Box, Button, Switch, TextField, Typography } from "@mui/material";
 
 function BoardShareModalElement() {
     return (
         <>
-            <Typography>비공유</Typography>
-            <Switch />
-            <Typography>공유</Typography>
+            <Box
+                sx={{
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                    alignItems: 'center',
+                    width: '100%',
+                }}
+            >
+                <Typography>공유 허용</Typography>
+                <Switch />
+            </Box>
+            <Box
+                sx={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    width: '100%',
+                    mb: 2,
+                    gap: '5px',
+                }}
+            >
+                <TextField
+                    size="small"
+                    value={window.location.href}
+                    fullWidth
+                    disabled
+                />
+                <Button
+                    variant="contained"
+                    sx={{
+                        flexShrink: 0,
+                        height: '40px',
+                    }}
+                >
+                    링크 복사
+                </Button>
+            </Box >
         </>
     );
 }
