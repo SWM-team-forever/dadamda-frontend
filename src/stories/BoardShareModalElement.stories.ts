67a6d181505ad0b24a13add62512b0be8978fe1c
ShinYoung-Kim <lukey0515@naver.com>
2023-10-18 22:34:46 +0900

    [DAD-1007] feat: 보드 공유하기 모달 스토리북 추가

---
+++ src/stories/BoardShareModalElement.stories.ts
@@ -0,0 +1,8 @@
+import BoardShareModalElement from "@/components/atoms/Modal/BoardShareModalElement";
+
+export default {
+    title: 'Atoms/BoardShareModalElement',
+    component: BoardShareModalElement
+}
+
+export const BoardShareModalElementStory = {};
