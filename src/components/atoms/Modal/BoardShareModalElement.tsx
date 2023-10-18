9914519638b94bdb951fb01ccd6699801338eb5f
ShinYoung-Kim <lukey0515@naver.com>
2023-10-18 21:51:46 +0900

    [DAD-1007] feat: 보드 공유/비공유 토글 버튼 공유 모달에 추가

--- src/components/atoms/Modal/BoardShareModalElement.tsx
+++ src/components/atoms/Modal/BoardShareModalElement.tsx
@@ -1,8 +1,12 @@
+import { Switch, Typography } from "@mui/material";
+
 function BoardShareModalElement() {
     return (
-        <div>
-
-        </div>
+        <>
+            <Typography>비공유</Typography>
+            <Switch />
+            <Typography>공유</Typography>
+        </>
     );
 }
 
