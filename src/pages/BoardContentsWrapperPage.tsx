e5a549d2775d649cfe1da358c5dadd06ea1e5a2c
ShinYoung-Kim <lukey0515@naver.com>
2023-10-18 20:49:40 +0900

    [DAD-1031] feat: 보드 state 존재여부 판단 함수 생성

--- src/pages/BoardContentsWrapperPage.tsx
+++ src/pages/BoardContentsWrapperPage.tsx
@@ -1,7 +1,14 @@
 import { RequireAuth } from "@/context/LoginContext";
 import BoardInfoPage from "@/pages/BoardInfoPage";
+import { useSearchParams } from "react-router-dom";
 
 function BoardContentsWrapperPage() {
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    function hasBoardStateInURL() {
+        return searchParams.has('bs');
+    }
+
     return (
         <RequireAuth>
             <BoardInfoPage />
