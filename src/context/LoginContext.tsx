7e4c00ce1808b7cac9397db0b9f0081b5a78bcc6
ShinYoung-Kim <lukey0515@naver.com>
2023-10-25 15:47:54 +0900

    [DAD-1009] feat: 토큰 유효성 검사 hook 생성

--- src/context/LoginContext.tsx
+++ src/context/LoginContext.tsx
@@ -1,6 +1,6 @@
 import { createContext, useContext, useState } from "react";
 import { Navigate, useLocation } from "react-router-dom";
-import { useLogout } from "@/hooks/useAccount";
+import { useGetToken, useLogout, useVerifyToken } from "@/hooks/useAccount";
 import { useDefaultSnackbar } from "@/hooks/useWarningSnackbar";
 
 export const LoginContext = createContext({
@@ -14,8 +14,6 @@ export function LoginProvider({ children }: { children: React.ReactNode }) {
         token: localStorage.getItem('token'),
     });
 
-    // const value: any = useMemo(() => ({ userInformation, setUserInformation }), [userInformation, setUserInformation]);
-
     return <LoginContext.Provider value={userInformation}>{children}</LoginContext.Provider>;
 }
 
@@ -28,23 +26,6 @@ export function useLoginState() {
     return context;
 }
 
-function verifyToken(token: string | null) {
-    if (!token) {
-        return false;
-    }
-
-    const tokenData = JSON.parse(atob(token.split('.')[1]));
-
-    if (tokenData.exp) {
-        const expirationDate = new Date(tokenData.exp * 1000);
-        const currentDate = new Date();
-
-        return expirationDate > currentDate;
-    }
-
-    return false;
-}
-
 export async function useHandleUnVerifiedTokenUser() {
     const logout = useLogout();
     await useDefaultSnackbar('다시 로그인해주세요.', 'error');
@@ -52,10 +33,8 @@ export async function useHandleUnVerifiedTokenUser() {
 }
 
 export function RequireAuth({ children }: { children: React.ReactNode }) {
-    const token = localStorage.getItem('token');
+    const token = useGetToken();
     const location = useLocation();
-
-    !verifyToken(token) && useHandleUnVerifiedTokenUser();
 
     return token ? (
         children
--- src/hooks/useAccount.ts
+++ src/hooks/useAccount.ts
@@ -1,6 +1,8 @@
 import { logEvent } from "@/utility/amplitude";
 import { useNavigate } from "react-router-dom";
 
+export const HAS_NO_ACCESS_ERROR = 'HAS_NO_ACCESS_ERROR';
+
 export function useLogout() {
     const navigate = useNavigate();
     logEvent('logout');
@@ -11,6 +13,36 @@ export function useLogout() {
     }
 }
 
-export function useGetToken() {
-    return localStorage.getItem('token');
+function getDecodedToken(token: string) {
+    return JSON.parse(atob(token.split('.')[1]));
+}
+
+function isTokenExpired(token: string) {
+    const tokenData = getDecodedToken(token);
+
+    if (tokenData.exp) {
+        const expirationDate = new Date(tokenData.exp * 1000);
+        const currentDate = new Date();
+
+        return expirationDate >= currentDate;
+    }
+
+    return true;
+}
+
+export function useVerifyToken(token: string | null) {
+    if (!token || !isTokenExpired(token)) {
+        throw new Error(HAS_NO_ACCESS_ERROR);
+    }
+}
+
+export function useGetToken(): string {
+    const token = localStorage.getItem('token');
+    if (!token) {
+        throw new Error(HAS_NO_ACCESS_ERROR);
+    }
+    
+    useVerifyToken(token);
+
+    return token;
 }
