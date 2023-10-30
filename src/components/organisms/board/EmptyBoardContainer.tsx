9432cfdae882cd674fdeb110b3ecda453327fd9f
ShinYoung-Kim <lukey0515@naver.com>
2023-10-30 16:55:51 +0900

    [DAD-1093] feat: emptyboard container 구현

--- src/components/organisms/board/EmptyBoardContainer.tsx
+++ src/components/organisms/board/EmptyBoardContainer.tsx
@@ -1,8 +1,26 @@
+import { Box, Typography } from "@mui/material";
+
+import { EmptyBoardIcon } from "@/components/atoms/Icon";
+import CreateBoardButton from "@/components/atoms/Board/CreateBoardButton";
+
 function EmptyBoardContainer() {
     return (
-        <div>
-
-        </div>
+        <Box
+            sx={{
+                position: 'absolute',
+                top: '50%',
+                left: '50%',
+                transform: 'translate(-50%, -50%)',
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                gap: '16px',
+            }}
+        >
+            <EmptyBoardIcon width="86" height="78" />
+            <Typography>보드를 추가해주세요!</Typography>
+            <CreateBoardButton />
+        </Box>
     );
 }
 
