19c31fbbeebdb9e1b30c1226814a007934d55ee0
ShinYoung-Kim <lukey0515@naver.com>
2023-10-30 16:11:14 +0900

    [DAD-1077] feat: 검색할 때 스크랩 id 제거

--- src/hooks/useSearch.ts
+++ src/hooks/useSearch.ts
@@ -1,9 +1,13 @@
 import searchAtom from "@/state/searchAtom";
 import { useAtom } from "jotai";
+import { useSearchParams } from "react-router-dom";
 
 export const useSearch = () => {
     const [search, setSearch] = useAtom(searchAtom);
+    const [searchParam, setSearchParam] = useSearchParams();
     const searchKeyword = ({keyword, type}: {keyword: string, type: string}) => {
+        searchParam.delete('scrapId');
+        setSearchParam(searchParam);
         setSearch({
             keyword: keyword,
             isSearched: true,
