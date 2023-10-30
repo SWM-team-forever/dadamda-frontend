d1c9efc9dba390d984e89fbcc0723ce9aab46fda
ShinYoung-Kim <lukey0515@naver.com>
2023-10-30 15:02:07 +0900

    [DAD-1077] feat: search atom 적용

--- src/components/molcules/SearchBar.tsx
+++ src/components/molcules/SearchBar.tsx
@@ -1,94 +1,96 @@
 import { Box, Button, InputBase } from "@mui/material";
-
-import { useEffect, useState } from "react";
-import { useSearchParams } from "react-router-dom";
+import { useState } from "react";
 
 import theme from "@/assets/styles/theme";
 import { useIsBlank } from "@/hooks/useValidation";
 import { logEvent } from "@/utility/amplitude";
 
-import { SearchIcon } from "@/components/atoms/Icon";
+import { MoveBackIcon, SearchIcon } from "@/components/atoms/Icon";
+import { useSearch } from "@/hooks/useSearch";
 
 function SearchBar({ type }: { type: string }) {
-    const [searchText, setSearchText] = useState('');
-    const [searchParams, setSearchParams] = useSearchParams();
+    const { search, searchKeyword, undoSearch } = useSearch();
+    const [searchText, setSearchText] = useState(search.keyword || '');
 
-    function isSearchTemplate() {
-        return searchParams.has('keyword');
+    function handleSearchButton() {
+        logEvent(`search_${type}`, { keyword: searchText });
+        searchKeyword({ keyword: searchText, type: type });
     }
 
-    function getKeyword() {
-        return searchParams.get('keyword');
+    function handleUndoSearchButton() {
+        logEvent(`erase_search`);
+        undoSearch();
+        setSearchText('');
     }
 
-    const buttonInfo = {
-        isSearched: {
-            text: '지우기',
-            action: () => {
-                logEvent(`erase_search`);
-                setSearchText('');
-                searchParams.delete('keyword');
-                setSearchParams(searchParams);
-            }
-        },
-        isNotSearched: {
-            text: '검색',
-            action: () => {
-                logEvent(`search_${type}`, { keyword: searchText });
-                searchParams.append('keyword', searchText);
-                setSearchParams(searchParams);
-            }
-        }
-    };
-
-    const isValidationSuccess = () => useIsBlank(searchText) ? false : true;
+    const isValidationSuccess = () => {
+        return useIsBlank(searchText) ? false : true;
+    }
 
     return (
         <Box
             sx={{
-                width: '100%',
-                borderRadius: '6px',
-                background: theme.color.Gray_020,
-                p: '10px',
-                boxSizing: 'border-box',
                 display: 'flex',
-                justifyContent: 'center',
                 alignItems: 'center',
+                gap: '8px',
             }}
         >
-            <InputBase
+            {
+                search.isSearched && <Box
+                    onClick={handleUndoSearchButton}
+                    sx={{
+                        cursor: 'pointer',
+                    }}
+                >
+                    <MoveBackIcon width="16" height="12" fill={theme.color.Gray_070} />
+                </Box>
+            }
+            <Box
                 sx={{
-                    flex: 1,
-                    gap: '8px',
+                    width: '100%',
+                    borderRadius: '6px',
+                    background: theme.color.Gray_020,
+                    p: '10px',
+                    boxSizing: 'border-box',
+                    display: 'flex',
+                    justifyContent: 'center',
                     alignItems: 'center',
-                    '& .MuiInputBase-input': {
-                        color: theme.color.Gray_070,
-                        fontWeight: '500',
-                        lineHeight: '150%',
-                        fontSize: '14px',
-                        p: '0',
-                    }
                 }}
-                value={isSearchTemplate() ? getKeyword() : searchText}
-                placeholder="검색"
-                inputProps={{ 'aria-label': 'search google maps' }}
-                startAdornment={
-                    <SearchIcon width="16" height="16" fill={theme.color.Gray_070} />
-                }
-                onChange={(e) => { setSearchText(e.target.value) }}
-                endAdornment={
-                    <Button
-                        sx={{
-                            p: '0',
+            >
+                <InputBase
+                    sx={{
+                        flex: 1,
+                        gap: '8px',
+                        alignItems: 'center',
+                        '& .MuiInputBase-input': {
                             color: theme.color.Gray_070,
-                        }}
-                        disabled={!isValidationSuccess() && !isSearchTemplate()}
-                        onClick={isSearchTemplate() ? buttonInfo.isSearched.action : buttonInfo.isNotSearched.action}
-                    >
-                        {isSearchTemplate() ? buttonInfo.isSearched.text : buttonInfo.isNotSearched.text}
-                    </Button>
-                }
-            />
+                            fontWeight: '500',
+                            lineHeight: '150%',
+                            fontSize: '14px',
+                            p: '0',
+                        }
+                    }}
+                    value={searchText}
+                    placeholder="검색어를 입력하세요."
+                    inputProps={{ 'aria-label': 'search google maps' }}
+                    startAdornment={
+                        <SearchIcon width="16" height="16" fill={theme.color.Gray_070} />
+                    }
+                    onChange={(e) => { setSearchText(e.target.value) }}
+                    endAdornment={
+                        <Button
+                            sx={{
+                                p: '0',
+                                color: theme.color.Gray_070,
+                            }}
+                            disabled={!isValidationSuccess()}
+                            onClick={handleSearchButton}
+                        >
+                            검색
+                        </Button>
+                    }
+                />
+            </Box>
         </Box>
     )
 }
