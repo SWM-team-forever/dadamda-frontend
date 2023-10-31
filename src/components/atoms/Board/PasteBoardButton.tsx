0d00101fb279c29bee01279db5820db75579710c
ShinYoung-Kim <lukey0515@naver.com>
2023-10-31 10:50:04 +0900

    [DAD-1059] feat: paste board button ui 구현

--- src/components/atoms/Board/PasteBoardButton.tsx
+++ src/components/atoms/Board/PasteBoardButton.tsx
@@ -1,8 +1,26 @@
+import theme from "@/assets/styles/theme";
+import { PasteIcon } from "@/components/atoms/Icon";
+import { Box, Typography } from "@mui/material";
+
 function PasteBoardButton() {
     return (
-        <div>
-
-        </div>
+        <Box
+            sx={{
+                display: 'flex',
+                color: theme.color.Gray_070,
+                gap: '5px',
+                alignItems: 'center',
+            }}
+        >
+            <Box
+                sx={{
+                    cursor: 'pointer',
+                }}
+            >
+                <PasteIcon width="20" height="20" fill={theme.color.Gray_070} />
+            </Box>
+            <Typography variant="h5">내 보드에 담기</Typography>
+        </Box>
     );
 }
 
