c0d46fe387b780abd391ff76fe5d11077d1be282
ShinYoung-Kim <lukey0515@naver.com>
2023-10-31 18:49:09 +0900

    [DAD-1149] refactor: 불필요한 import 제거

--- src/pages/OpenBoardPage.tsx
+++ src/pages/OpenBoardPage.tsx
@@ -2,7 +2,6 @@ import { Box, Typography } from "@mui/material";
 import { useParams } from "react-router-dom";
 
 import { useGetOpenBoardTitle } from "@/api/board";
-import MetaTag from "@/utility/MetaTag";
 
 import { TrashableItems } from "@/components/templates/TrashableItems";
 import CopyBoardButton from "@/components/atoms/Board/CopyBoardButton";
