a5d21e50f8e9a7f861872020c6c3705142a82aea
ShinYoung-Kim <lukey0515@naver.com>
2023-11-01 14:46:44 +0900

    [DAD-1134] style: 보드 가이드 버튼 styling

--- src/components/atoms/Board/BoardGuideButton.tsx
+++ src/components/atoms/Board/BoardGuideButton.tsx
@@ -1,8 +1,19 @@
+import theme from "@/assets/styles/theme";
+import { BoardGuideIcon } from "@/components/atoms/Icon";
+import { Button } from "@mui/material";
+
 function BoardGuideButton() {
     return (
-        <div>
-
-        </div>
+        <Button
+            variant="contained"
+            sx={{
+                color: theme.color.Gray_080,
+                backgroundColor: 'white',
+            }}
+            startIcon={<BoardGuideIcon width="12" height="12" fill={theme.color.Gray_080} />}
+        >
+            보드 이용 가이드
+        </Button>
     );
 }
 
