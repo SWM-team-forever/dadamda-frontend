6c59aa3d9561546ee5756faf7f9e8f63f5463805
ShinYoung-Kim <lukey0515@naver.com>
2023-11-01 15:01:24 +0900

    [DAD-1134] feat: 보드 가이드 버튼 기능 연결

--- src/components/atoms/Board/BoardGuideButton.tsx
+++ src/components/atoms/Board/BoardGuideButton.tsx
@@ -3,14 +3,23 @@ import { BoardGuideIcon } from "@/components/atoms/Icon";
 import { Button } from "@mui/material";
 
 function BoardGuideButton() {
+    const BOARD_GUIDE_LINK = 'https://gapyeong.notion.site/DADAMDA-c31a77d5d49f49898d965ee7e705442a?pvs=4';
+    const handleOpenBoardGuide = () => {
+        window.open(BOARD_GUIDE_LINK, '_blank');
+    }
+
     return (
         <Button
             variant="contained"
             sx={{
                 color: theme.color.Gray_080,
                 backgroundColor: 'white',
+                '&:hover': {
+                    backgroundColor: theme.color.Gray_020,
+                },
             }}
             startIcon={<BoardGuideIcon width="12" height="12" fill={theme.color.Gray_080} />}
+            onClick={handleOpenBoardGuide}
         >
             보드 이용 가이드
         </Button>
