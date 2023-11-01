bc87c7c7f448cac39d5028e56a499d28287c41dc
ShinYoung-Kim <lukey0515@naver.com>
2023-11-01 20:23:48 +0900

    [DAD-1135] feat: 보드 이미지 업로드 validation 구현

--- src/components/atoms/ImageUploadButton.tsx
+++ src/components/atoms/ImageUploadButton.tsx
@@ -1,5 +1,6 @@
 import DefaultBoardThumbnail from "@/components/atoms/Board/DefaultBoardThumbnail";
 import useGetPreviewFile from "@/hooks/useGetPrevieFile";
+import { IMAGE_FILE_SIZE_LIMITATION, useIsFileSizeLessThanLimitation, useIsFileTypeImage } from "@/hooks/useValidation";
 import { Box } from "@mui/material";
 import { useEffect, useState } from "react";
 
@@ -16,8 +17,27 @@ function ImageUploadButton({ imageURL }: ImageUploadButtonProps) {
             return;
         }
 
+        const validateImageErrorMessage = getValidateImageErrorMessage(file);
+        if (validateImageErrorMessage) {
+            alert(validateImageErrorMessage);
+            return;
+        }
+
         previewUploadImage(file);
     }, [file]);
+
+    const getValidateImageErrorMessage = (file: File) => {
+        let error = '';
+        if (!useIsFileSizeLessThanLimitation(file, IMAGE_FILE_SIZE_LIMITATION)) {
+            error = '파일 용량이 너무 큽니다.';
+        }
+
+        if (!useIsFileTypeImage(file)) {
+            error = '이미지 파일만 업로드 가능합니다.';
+        }
+
+        return error;
+    }
 
     const previewUploadImage = (file: File) => {
         const reader = new FileReader();
--- src/hooks/useValidation.ts
+++ src/hooks/useValidation.ts
@@ -9,28 +9,41 @@ export const MAX_SCRAP_PRICE_LENGTH = 100;
 export const MAX_SCRAP_CHANNELNAME_LENGTH = 100;
 export const MAX_BOARD_DESCRIPTION_LENGTH = 1000;
 export const MAX_BOARD_TITLE_LENGTH = 50;
+export const IMAGE_FILE_SIZE_LIMITATION = 1024 * 1024 * 10;
 
 export function useIsValidURL(url: string) {
-    try {
-        new URL(url);
-        return true;
-    } catch (err) {
-        return false;
-    }
+	try {
+		new URL(url);
+		return true;
+	} catch (err) {
+		return false;
+	}
 }
 
 export function useIsLessThanLengthLimitation(text: string, limit: number) {
-    return text.length <= limit;
+	return text.length <= limit;
 }
 
 export function useIsBlank(text: string) {
-    return /^\s*$/.test(text);
+	return /^\s*$/.test(text);
 }
 
 export function useIsEntered(text: string) {
-    return text.length > 0;
+	return text.length > 0;
 }
 
 export function useIsWhiteSpaceExist(text: string) {
-    return !/^\S*$/.test(text);
+	return !/^\S*$/.test(text);
+}
+
+export function useIsFileSizeLessThanLimitation(file: File, limit: number) {
+	return file.size <= limit;
+}
+
+export function useIsFileTypeImage(file: File) {
+	if (file.type.match(/^image\/(png|jpeg|jpg)$/)) {
+		return true;
+	}
+
+	return false;
 }
