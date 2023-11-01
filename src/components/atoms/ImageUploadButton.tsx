102ddc0befd36552abd8fe2f84c80f3532a5b468
ShinYoung-Kim <lukey0515@naver.com>
2023-11-01 19:55:37 +0900

    [DAD-1135] feat: 이미지 업로드 버튼에 useGetPreviewFile 적용

--- src/components/atoms/ImageUploadButton.tsx
+++ src/components/atoms/ImageUploadButton.tsx
@@ -1,8 +1,49 @@
-function ImageUploadButton() {
-    return (
-        <div>
+import DefaultBoardThumbnail from "@/components/atoms/Board/DefaultBoardThumbnail";
+import useGetPreviewFile from "@/hooks/useGetPrevieFile";
+import { Box } from "@mui/material";
+import { useEffect, useState } from "react";
 
-        </div>
+interface ImageUploadButtonProps {
+    imageURL?: string;
+}
+
+function ImageUploadButton({ imageURL }: ImageUploadButtonProps) {
+    const [requestPrevieFile, file] = useGetPreviewFile();
+    const [image, setImage] = useState(imageURL);
+
+    useEffect(() => {
+        if (!file) {
+            return;
+        }
+
+        previewUploadImage(file);
+    }, [file]);
+
+    const previewUploadImage = (file: File) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onloadend = () => {
+            setImage(reader.result as string);
+        }
+    }
+
+    return (
+        <>
+            {image
+                ? <img
+                    src={image}
+                    style={{
+                        width: '100%',
+                        aspectRatio: '16/9',
+                    }}
+                />
+                : <Box
+                    onClick={requestPrevieFile}
+                >
+                    <DefaultBoardThumbnail />
+                </Box>
+            }
+        </>
     );
 }
 
