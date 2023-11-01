334c0c422eba2b9d5c8c548c5d36d56edd8e148a
ShinYoung-Kim <lukey0515@naver.com>
2023-11-01 18:09:51 +0900

    [DAD-1135] style: 디폴트 보드 썸네일 꾸미기

--- src/components/atoms/Board/DefaultBoardThumbnail.tsx
+++ src/components/atoms/Board/DefaultBoardThumbnail.tsx
@@ -1,8 +1,27 @@
+import { Box } from "@mui/material";
+
+import theme from "@/assets/styles/theme";
+
+import { ProfileIcon } from "@/components/atoms/Icon";
+
 function DefaultBoardThumbnail() {
     return (
-        <div>
-
-        </div>
+        <Box
+            sx={{
+                width: '100%',
+                aspectRatio: '16/9',
+                backgroundColor: theme.color.Blue_090,
+                cursor: 'pointer',
+                '&:hover': {
+                    backgroundColor: theme.color.Blue_080,
+                },
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+            }}
+        >
+            <ProfileIcon size='80' />
+        </Box>
     );
 }
 
