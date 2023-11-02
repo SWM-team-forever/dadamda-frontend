4378766804ee12e7dd5f9565e894992464d01b11
ShinYoung-Kim <lukey0515@naver.com>
2023-11-02 15:27:19 +0900

    [DAD-1137] feat: 유저 페이지 필요한 date 계산 추가

--- src/hooks/useCalculateDateDiff.ts
+++ src/hooks/useCalculateDateDiff.ts
@@ -1,10 +1,10 @@
-import dayjs from 'dayjs';
-import duration from 'dayjs/plugin/duration';
-import utc from 'dayjs/plugin/utc';
-import timezone from 'dayjs/plugin/timezone';
+import dayjs from "dayjs";
+import duration from "dayjs/plugin/duration";
+import utc from "dayjs/plugin/utc";
+import timezone from "dayjs/plugin/timezone";
 
 import relativeTime from "dayjs/plugin/relativeTime";
-import "dayjs/locale/ko"; 
+import "dayjs/locale/ko";
 
 dayjs.extend(duration);
 dayjs.extend(utc);
@@ -12,11 +12,18 @@ dayjs.extend(timezone);
 dayjs.extend(relativeTime);
 dayjs.locale("ko");
 
-
 export function getTimeDiff(publishedDate: number): string {
-    return dayjs.unix(publishedDate).fromNow();
+	return dayjs.unix(publishedDate).fromNow();
 }
 
 export function useGetCurrentTimeInUnixTime() {
-    return dayjs().unix();
+	return dayjs().unix();
+}
+
+export function useConvertUnixTimeToDateFormat(unixTime: number): string {
+	return dayjs.unix(unixTime).format("YYYY년 MM월 DD일");
+}
+
+export function useGetDaysDiff(unixTime: number): number {
+	return dayjs().diff(dayjs.unix(unixTime), "day");
 }
