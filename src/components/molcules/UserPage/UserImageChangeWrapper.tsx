689be4dde5875bd7d8b850e53bbc03140835315b
ShinYoung-Kim <lukey0515@naver.com>
2023-11-02 20:20:04 +0900

    [DAD-1138] feat: 유저 이미지 선택 기능 구현

--- src/components/molcules/UserPage/UserImageChangeWrapper.tsx
+++ src/components/molcules/UserPage/UserImageChangeWrapper.tsx
@@ -1,18 +1,94 @@
-import { grayOutlinedButtonStyle, grayFullfilledButtonStyle, useIsUserPageEditMode } from "@/pages/UserPage";
-import { Box, Button } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
+
 import defaultUserImage from '@/assets/images/Avatar.png';
+import useGetPreviewFile from "@/hooks/useGetPrevieFile";
+
+import { grayOutlinedButtonStyle, grayFullfilledButtonStyle, useIsUserPageEditMode } from "@/pages/UserPage";
+import { IMAGE_FILE_SIZE_LIMITATION, useIsFileSizeLessThanLimitation, useIsFileTypeImage } from "@/hooks/useValidation";
+import theme from "@/assets/styles/theme";
 
 function UserImageChangeWrapper({ mode, profileUrl }: { mode: string, profileUrl?: string }) {
+    const [requestPrevieFile, file] = useGetPreviewFile();
+    const [image, setImage] = useState(profileUrl);
+
+    useEffect(() => {
+        if (!file) {
+            return;
+        }
+
+        const validateImageErrorMessage = getValidateImageErrorMessage(file);
+        if (validateImageErrorMessage) {
+            alert(validateImageErrorMessage);
+            return;
+        }
+
+        previewUploadImage(file);
+    }, [file]);
+
+    const getValidateImageErrorMessage = (file: File) => {
+        let error = '';
+        if (!useIsFileSizeLessThanLimitation(file, IMAGE_FILE_SIZE_LIMITATION)) {
+            error = '파일 용량이 너무 큽니다.';
+        }
+
+        if (!useIsFileTypeImage(file)) {
+            error = '이미지 파일만 업로드 가능합니다.';
+        }
+
+        return error;
+    }
+
+    const previewUploadImage = (file: File) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onloadend = () => {
+            setImage(reader.result as string);
+        }
+    }
+
+    const handleRemoveImage = () => {
+        setImage('');
+    }
+
     function ProfileImage({ src }: { src: string }) {
-        return <img
-            src={src}
-            alt="프로필 이미지"
-            style={{
-                width: '200px',
-                height: '200px',
-                borderRadius: '100%',
+        return <Box
+            onClick={requestPrevieFile}
+            sx={{
+                position: 'relative',
+                cursor: 'pointer',
+                '&:hover .MuiTypography-root': {
+                    display: 'block',
+                },
+                '&:hover .profile-image': {
+                    filter: 'brightness(0.5)',
+                },
             }}
-        />
+        >
+            <img
+                src={src}
+                alt="프로필 이미지"
+                style={{
+                    width: '200px',
+                    height: '200px',
+                    borderRadius: '100%',
+                }}
+                className="profile-image"
+            />
+            <Typography
+                sx={{
+                    position: 'absolute',
+                    top: '50%',
+                    left: '50%',
+                    transform: 'translate(-50%, -50%)',
+                    display: 'none',
+                    zIndex: 1,
+                    color: theme.color.Blue_050,
+                }}
+            >
+                이미지 선택
+            </Typography>
+        </Box>
     }
     return (
         <Box
@@ -22,8 +98,8 @@ function UserImageChangeWrapper({ mode, profileUrl }: { mode: string, profileUrl
             }}
         >
             <Box>
-                {profileUrl
-                    ? <ProfileImage src={profileUrl} />
+                {image
+                    ? <ProfileImage src={image} />
                     : <ProfileImage src={defaultUserImage} />
                 }
                 {useIsUserPageEditMode(mode) &&
@@ -36,8 +112,17 @@ function UserImageChangeWrapper({ mode, profileUrl }: { mode: string, profileUrl
                             mt: '24px',
                         }}
                     >
-                        <Button sx={grayOutlinedButtonStyle}>이미지 변경</Button>
-                        <Button sx={grayFullfilledButtonStyle}>이미지 삭제</Button>
+                        <Button
+                            sx={grayOutlinedButtonStyle}
+                        >
+                            이미지 변경
+                        </Button>
+                        <Button
+                            sx={grayFullfilledButtonStyle}
+                            onClick={handleRemoveImage}
+                        >
+                            이미지 삭제
+                        </Button>
                     </Box>
                 }
             </Box>
