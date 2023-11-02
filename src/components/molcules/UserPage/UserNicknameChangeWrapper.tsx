9c885af09ba753c154ba36beefff603c2c7d7c9e
ShinYoung-Kim <lukey0515@naver.com>
2023-11-02 22:57:58 +0900

    [DAD-1138] feat: 유저 닉네임 변경 api 반영 및 validation 적용

--- src/components/molcules/UserPage/UserNicknameChangeWrapper.tsx
+++ src/components/molcules/UserPage/UserNicknameChangeWrapper.tsx
@@ -1,7 +1,8 @@
 import { useEditUserNickname } from "@/api/user";
 import theme from "@/assets/styles/theme";
+import { MAX_USER_NICKNAME_LENGTH, useIsBlank, useIsLessThanLengthLimitation } from "@/hooks/useValidation";
 import { useIsUserPageViewMode } from "@/pages/UserPage";
-import { Box, Typography, OutlinedInput, Button } from "@mui/material";
+import { Box, Typography, OutlinedInput, Button, FormHelperText } from "@mui/material";
 import { useState } from "react";
 
 interface UserNicknameChangeWrapperProps {
@@ -16,11 +17,33 @@ function UserNicknameChangeWrapper({ mode, nickname, changeModeIntoView }: UserN
         setNicknameInput(event.target.value);
     }
 
-    const { editUserNicknameMutate } = useEditUserNickname();
+    const { mutate, error } = useEditUserNickname();
+    const isNicknameExist = (error: any) => error.message === 'BR003';
     const handleEditUserNickname = () => {
-        changeModeIntoView();
-        editUserNicknameMutate(nicknameInput);
+        mutate(nicknameInput);
+        if (isNicknameExist(error)) {
+            return;
+        }
+        changeModeIntoView()
     }
+
+    const validateUserNickname = () => {
+        if (useIsBlank(nicknameInput)) {
+            return '닉네임을 입력해주세요.';
+        }
+
+        if (!useIsLessThanLengthLimitation(nicknameInput, MAX_USER_NICKNAME_LENGTH)) {
+            return '닉네임은 10글자 이하로 입력해주세요.';
+        }
+
+        if (!isNicknameInputChanged()) {
+            return '닉네임이 변경되지 않았습니다.';
+        }
+
+        return;
+    }
+
+    const isNicknameInputChanged = () => nicknameInput !== nickname;
 
     if (useIsUserPageViewMode(mode)) {
         return;
@@ -35,42 +58,53 @@ function UserNicknameChangeWrapper({ mode, nickname, changeModeIntoView }: UserN
                 width: '100%',
             }}
         >
-            <Box
-                sx={{
-                    display: 'flex',
-                    alignItems: 'center',
-                    gap: '13px',
-                    mt: '100px',
-                    width: '100%',
-                    justifyContent: 'center',
-                }}
-            >
-                <Typography
+            <Box>
+                <Box
                     sx={{
-                        fontWeight: '600',
-                        fontSize: '16px',
-                        lineHeight: '150%',
-                        color: theme.color.Gray_090,
+                        display: 'flex',
+                        alignItems: 'center',
+                        gap: '13px',
+                        mt: '100px',
+                        width: '100%',
+                        justifyContent: 'center',
                     }}
                 >
-                    닉네임:
-                </Typography>
-                <OutlinedInput
-                    value={nicknameInput}
+                    <Typography
+                        sx={{
+                            fontWeight: '600',
+                            fontSize: '16px',
+                            lineHeight: '150%',
+                            color: theme.color.Gray_090,
+                        }}
+                    >
+                        닉네임:
+                    </Typography>
+                    <OutlinedInput
+                        value={nicknameInput}
+                        sx={{
+                            maxWidth: {
+                                xs: '215px',
+                                sm: '260px',
+                            },
+                            'input': {
+                                p: '8px 0 8px 12px',
+                                backgroundColor: '#FFF',
+                                borderRadius: '8px',
+                                border: `1px solid ${theme.color.Gray_040}`
+                            }
+                        }}
+                        onChange={handleNicknameInputChange}
+                    />
+                </Box>
+                <FormHelperText
                     sx={{
-                        maxWidth: {
-                            xs: '215px',
-                            sm: '260px',
-                        },
-                        'input': {
-                            p: '8px 0 8px 12px',
-                            backgroundColor: '#FFF',
-                            borderRadius: '8px',
-                            border: `1px solid ${theme.color.Gray_040}`
-                        }
+                        width: '100%',
+                        display: 'flex',
+                        justifyContent: 'flex-end',
                     }}
-                    onChange={handleNicknameInputChange}
-                />
+                >
+                    {validateUserNickname()}
+                </FormHelperText>
             </Box>
             <Button
                 variant='contained'
@@ -79,6 +113,7 @@ function UserNicknameChangeWrapper({ mode, nickname, changeModeIntoView }: UserN
                     width: '180px',
                 }}
                 onClick={handleEditUserNickname}
+                disabled={!!validateUserNickname()}
             >
                 프로필 변경하기
             </Button>
