e7c985ed63b85ce0bbd5b8b0981dcf7381d92e9c
ShinYoung-Kim <lukey0515@naver.com>
2023-11-03 18:04:02 +0900

    [DAD-1054] feat: 보드 게시 버튼 UI 생성

---
+++ src/components/atoms/Board/BoardPublishButton.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@mui/material";
+
+function BoardPublishButton() {
+    return (
+        <Button
+            variant="contained"
+            size="large"
+        >
+            트렌딩 보드에 게시
+        </Button>
+    )
+}
+
+export default BoardPublishButton;
