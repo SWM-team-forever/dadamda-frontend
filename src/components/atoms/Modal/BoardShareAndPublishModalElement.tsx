10f3a3e1210447f716608a9e0e49a0150dad9022
ShinYoung-Kim <lukey0515@naver.com>
2023-11-03 17:17:08 +0900

    [DAD-1054] style: 보드 공유 게시하기 모달 탭 스타일링 적용

---
+++ src/components/atoms/Modal/BoardShareAndPublishModalElement.tsx
@@ -0,0 +1,66 @@
+import theme from "@/assets/styles/theme";
+import BoardPublishModalElement from "@/components/atoms/Modal/BoardPublishModalElement";
+import BoardShareModalElement from "@/components/atoms/Modal/BoardShareModalElement";
+import { Box, Tab, Tabs } from "@mui/material";
+import { useState } from "react";
+
+export const typographyStyle = {
+    fontSize: '14px',
+    fontWeight: '400',
+    lineHeight: '150%',
+    color: theme.color.Gray_090,
+}
+
+function BoardShareAndPublishModalElement() {
+    const [value, setValue] = useState(0);
+    const handleChangeValue = (event: React.SyntheticEvent, newValue: number) => {
+        setValue(newValue);
+    };
+
+    return (
+        <Box>
+            <Box
+                sx={{
+                    borderBottom: 1,
+                    borderColor: 'divider',
+                }}
+            >
+                <Tabs value={value} onChange={handleChangeValue}
+                    sx={{
+                        '& .MuiButtonBase-root': {
+                            minHeight: 'auto',
+                            color: theme.color.Gray_080,
+                            fontWeight: '400',
+                            fontSize: '14px',
+                        },
+                        '& .Mui-selected': {
+                            color: theme.color.Blue_080,
+                            fontWeight: '600'
+                        },
+                        minHeight: 'auto',
+                    }}
+                >
+                    <Tab label="공유" sx={{
+                        minWidth: 'auto',
+                        m: '0 10px 8px 0',
+                        p: '0',
+                        '& .MuiTouchRipple-root': {
+                            minHeight: '20px',
+                        },
+                    }} />
+                    <Tab label="게시"
+                        sx={{
+                            minWidth: 'auto',
+                            m: '0 0 8px 10px',
+                            p: '0',
+                        }}
+                    />
+                </Tabs>
+            </Box>
+            <BoardShareModalElement />
+            <BoardPublishModalElement />
+        </Box>
+    )
+}
+
+export default BoardShareAndPublishModalElement;
