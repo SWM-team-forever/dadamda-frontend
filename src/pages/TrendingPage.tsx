3bd9555970b4806b6fe53740202ecf05dc63226b
ShinYoung-Kim <lukey0515@naver.com>
2023-11-05 11:53:03 +0900

    [DAD-1139] feat: 트렌딩 페이지 레이아웃 구성

--- src/pages/TrendingPage.tsx
+++ src/pages/TrendingPage.tsx
@@ -1,10 +1,44 @@
+import TrendingCard, { TrendingCardProps } from '@/components/molcules/Trending/TrendingCard';
+import { Box } from '@mui/material';
+import { trendingMockData } from '__mocks__/trendingMockData';
 import styled from 'styled-components';
 
 function TrendingPage() {
     return (
         <PageWrapper>
-            아직 지원하지 않는 기능입니다.
-        </PageWrapper>
+            <Box
+                sx={{
+                    width: {
+                        xs: '100%',
+                        sm: '650px',
+                    },
+                    height: '100%',
+                    gap: '24px',
+                    p: {
+                        xs: '12px 24px',
+                        sm: '12px 0',
+                    },
+                    boxSizing: 'border-box',
+                }}
+            >
+                {trendingMockData.map((data: TrendingCardProps, index: number) => (
+                    <TrendingCard
+                        key={index}
+                        profileUrl={data.profileUrl}
+                        nickname={data.nickname}
+                        title={data.title}
+                        description={data.description}
+                        tag={data.tag}
+                        heartCnt={data.heartCnt}
+                        shareCnt={data.shareCnt}
+                        viewCnt={data.viewCnt}
+                        createdAt={data.createdAt}
+                        thumbnailUrl={data.thumbnailUrl}
+                        contents={data.contents}
+                    />
+                ))}
+            </Box>
+        </PageWrapper >
     );
 }
 
@@ -14,6 +48,7 @@ const PageWrapper = styled.div`
     height: calc(100vh - 56px);
     justify-content: center;
     align-items: center;
+    overflow: auto;
 `
 
 export default TrendingPage;
