4baa283c86654c95bf14e3e9ad6fa7e038847c4b
ShinYoung-Kim <lukey0515@naver.com>
2023-11-06 02:11:32 +0900

    [DAD-1139] feat: 왼쪽 토픽 사이드바 추가

--- src/pages/TrendingPage.tsx
+++ src/pages/TrendingPage.tsx
@@ -1,42 +1,132 @@
+import theme from '@/assets/styles/theme';
 import TrendingCard, { TrendingCardProps } from '@/components/molcules/Trending/TrendingCard';
-import { Box } from '@mui/material';
+import { Box, Button, Grid, Typography } from '@mui/material';
 import { trendingMockData } from '__mocks__/trendingMockData';
 import styled from 'styled-components';
 
 function TrendingPage() {
-    return (
-        <PageWrapper>
+    function Topic() {
+        const category = {
+            LIST: {
+                text: '전체',
+            },
+            ENTERTAINMENT_ART: {
+                text: '엔터테인먼트/예술',
+            },
+            HOBBY_TRAVEL: {
+                text: '취미/여가/여행',
+            },
+            LIFE_SHOPPING: {
+                text: '생활/노하우/쇼핑',
+            },
+            KNOWLEDGE_TREND: {
+                text: '지식/동향',
+            },
+        }
+
+        return (
             <Box
                 sx={{
-                    width: {
-                        xs: '100%',
-                        sm: '650px',
-                    },
-                    height: '100%',
-                    gap: '24px',
-                    p: {
-                        xs: '12px 24px',
-                        sm: '12px 0',
-                    },
+                    display: 'flex',
+                    flexDirection: 'column',
+                    gap: '10px',
+                    maxWidth: '230px',
+                    width: '100%',
+                    p: '12px',
                     boxSizing: 'border-box',
                 }}
             >
-                {trendingMockData.map((data: TrendingCardProps, index: number) => (
-                    <TrendingCard
-                        key={index}
-                        profileUrl={data.profileUrl}
-                        nickname={data.nickname}
-                        title={data.title}
-                        description={data.description}
-                        tag={data.tag}
-                        heartCnt={data.heartCnt}
-                        shareCnt={data.shareCnt}
-                        viewCnt={data.viewCnt}
-                        createdAt={data.createdAt}
-                        thumbnailUrl={data.thumbnailUrl}
-                        contents={data.contents}
-                    />
-                ))}
+                <Typography
+                    sx={{
+                        color: theme.color.text_gray_color,
+                        fontSize: '14px',
+                        lineHeight: '150%',
+                        fontWeight: '600',
+                    }}
+                >
+                    토픽
+                </Typography>
+                <Box
+                    sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'start',
+                        backgroundColor: theme.color.Gray_020,
+                        borderRadius: '8px',
+                        p: '16px',
+                        boxSizing: 'border-box',
+                    }}
+                >
+                    {Object.values(category).map((item: any, index: number) => {
+                        return <Button
+                            key={index}
+                            sx={{
+                                color: theme.color.Gray_080,
+                                fontSize: '14px',
+                                lineHeight: '150%',
+                                fontWeight: '400',
+                                '&:hover': {
+                                    backgroundColor: 'transparent',
+                                },
+                                minWidth: '0',
+                                width: '100%',
+                                justifyContent: 'flex-start',
+                            }}
+                        >
+                            {item.text}
+                        </Button>
+                    }
+                    )}
+                </Box>
+            </Box>
+        )
+    }
+
+    return (
+        <PageWrapper>
+            <Box
+                gridTemplateColumns='3fr 7fr 3fr'
+                columnGap='24px'
+                display={{
+                    xs: 'block',
+                    md: 'grid',
+                }}
+                sx={{
+                    width: 'auto',
+                    height: '100%',
+                }}
+            >
+                <Grid item
+                    sx={{
+                        display: {
+                            xs: 'none',
+                            md: 'flex',
+                        },
+                        maxWidth: '100%',
+                        justifyContent: 'end'
+                    }}
+                >
+                    <Topic />
+                </Grid>
+                <Grid item>
+                    {trendingMockData.map((data: TrendingCardProps, index: number) => (
+                        <TrendingCard
+                            key={index}
+                            profileUrl={data.profileUrl}
+                            nickname={data.nickname}
+                            title={data.title}
+                            description={data.description}
+                            tag={data.tag}
+                            heartCnt={data.heartCnt}
+                            shareCnt={data.shareCnt}
+                            viewCnt={data.viewCnt}
+                            createdAt={data.createdAt}
+                            thumbnailUrl={data.thumbnailUrl}
+                            contents={data.contents}
+                            uuid={data.uuid}
+                        />
+                    ))}
+                </Grid>
             </Box>
         </PageWrapper >
     );
