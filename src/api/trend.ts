f4e0ceecedc0ae11b55a2051ec392a08522a7bf8
ShinYoung-Kim <lukey0515@naver.com>
2023-11-07 14:08:54 +0900

    [DAD-1140] refactor: 불필요한 코드 제거

--- src/api/trend.ts
+++ src/api/trend.ts
@@ -14,9 +14,10 @@ const getTrendingList = async ({
 }) => {
 	const { currentTime, monthLaterTime } =
 		useGetCurrentTimeAndMonthLaterInSpecificDateFormat();
+
 	const urlWithoutTag =
 		GET_TRENDING_LIST_URL +
-		`?page=${pages}&size=${size}&startDate=${currentTime.toString()}&endDate=${monthLaterTime.toString()}`;
+		`?page=${pages}&size=${size}&startDate=${currentTime}&endDate=${monthLaterTime}`;
 	const requestUrl = tag ? urlWithoutTag + `&tag=${tag}` : urlWithoutTag;
 
 	const response = await fetch(requestUrl, {
@@ -41,36 +42,41 @@ export const useGetTrendingList = () => {
 	const { trending } = useTrendingAtom();
 	const isListTag = (tag: string) => tag === "LIST";
 
-	const { data, fetchNextPage, hasNextPage } = useInfiniteQuery(
-		["trendingList", trending.tag],
-		({ pageParam = 0 }) => {
-			console.log(trending.tag);
-			console.log(isListTag(trending.tag));
-			return isListTag(trending.tag)
-				? getTrendingList({
-						pages: pageParam,
-						size: 3,
-				  })
-				: getTrendingList({
-						pages: pageParam,
-						size: 3,
-						tag: trending.tag,
-				  });
-		},
-		{
-			getNextPageParam: (lastPage) => {
-				return lastPage.data.last
-					? undefined
-					: lastPage.data.number + 1;
+	const { data, fetchNextPage, hasNextPage, isLoading } =
+		useInfiniteQuery(
+			["trendingList", trending.tag],
+			({ pageParam = 0 }) => {
+				return isListTag(trending.tag)
+					? getTrendingList({
+							pages: pageParam,
+							size: 3,
+					  })
+					: getTrendingList({
+							pages: pageParam,
+							size: 3,
+							tag: trending.tag,
+					  });
 			},
-			retry: false,
-		}
-	);
+			{
+				getNextPageParam: (lastPage) => {
+					return lastPage.data.last
+						? undefined
+						: lastPage.data.number + 1;
+				},
+				retry: false,
+			}
+		);
 
-	const [trendingList, fetchNextTrendingList, hasNextTrendingList] = [
-		data,
-		fetchNextPage,
-		hasNextPage,
-	];
-	return { trendingList, fetchNextTrendingList, hasNextTrendingList };
+	const [
+		trendingList,
+		fetchNextTrendingList,
+		hasNextTrendingList,
+		isTrendingListLoading,
+	] = [data, fetchNextPage, hasNextPage, isLoading];
+	return {
+		trendingList,
+		fetchNextTrendingList,
+		hasNextTrendingList,
+		isTrendingListLoading,
+	};
 };
