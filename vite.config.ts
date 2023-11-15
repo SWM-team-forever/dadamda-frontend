7f8bb9ba63610be16c3f3f2c59369875e3204438
ShinYoung-Kim <lukey0515@naver.com>
2023-11-15 22:47:53 +0900

    [DAD-1226] fix: pwa 관련 설정 파일들 위치 변경

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,29 @@
 import { sentryVitePlugin } from "@sentry/vite-plugin";
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  base: "./",
-  plugins: [react(), tsconfigPaths(), sentryVitePlugin({
-    org: "dadamda",
-    project: "dadamda-fe"
-  })],
-  test: {
-    environment: "jsdom",
-  },
-  build: {
-    publicDir: 'public',
-    sourcemap: true
-  }
-})
+	base: "./",
+	plugins: [
+		react(),
+		tsconfigPaths(),
+		sentryVitePlugin({
+			org: "dadamda",
+			project: "dadamda-fe",
+		}),
+	],
+	test: {
+		environment: "jsdom",
+	},
+	build: {
+		publicDir: "public",
+		sourcemap: true,
+		rollupOptions: {
+			input: {
+				main: "./index.html",
+				sw: "./service-worker.js",
+			},
+		},
+	},
+});
