d2b7be3a1c6cfb4f7ea77420f056d74a647a5b75
ShinYoung-Kim <lukey0515@naver.com>
2023-11-23 12:25:49 +0900

    [DAD-1255] feat: 프로필 이미지 컴포넌트 기본 이미지 추가

--- src/components/atoms/ProfileImage.tsx
+++ src/components/atoms/ProfileImage.tsx
@@ -10,11 +10,11 @@ function ProfileImage({ source, size, onClick }: ProfileImageProps) {
   return (
     <ImageContainer onClick={onClick}>
       <Image
-        src={source}
+        src={source || '/default_image.png'}
         size={size}
         onError={e => {
           e.currentTarget.src = '/default_image.png';
-        }} 
+        }}
       />
     </ImageContainer>
   );
@@ -30,7 +30,7 @@ const Image = styled.img<{ size: number }>`
   ${props => `width: ${props.size}px`};
   ${props => `height: ${props.size}px`};
   border-radius: 100%;
-  cursor: pointer;
+  cursor: ${props => props.onClick && 'pointer'};
 `
 
 export default ProfileImage;
